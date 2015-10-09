92485ec9549ee2fed92bb4570a41dc2a55eaf27e
Tiago Fassoni <tiagofassoni@gmail.com>
2015-10-09 17:06:17 -0300

    Removed the stub test

--- tests/hello.test.js
+++
@@ -1,9 +0,0 @@
-var test = require('tape')
-var React = require('react/addons')
-// var TestUtils = React.addons.TestUtils
-var Hello = require('../')
-
-test('Hello#renderToStaticMarkup', function (t) {
-  t.equal(React.renderToStaticMarkup(Hello()), '<div>Hello</div>', 'Renders to static markup')
-  t.end()
-})
