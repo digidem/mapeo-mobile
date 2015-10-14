4a23da16a3db06d35ee2682871c30c020f64d876
Tiago Fassoni <tiagofassoni@gmail.com>
2015-10-14 15:26:55 -0300

    Adds tests for the redux store

--- tests/redux-store.test.js
+++ tests/redux-store.test.js
@@ -1,21 +1,81 @@
 let test = require('tape')
 import store from '../redux/store'
 import * as actions from '../redux/actions/actions'
+import Immutable from 'immutable'
 
 test('Store testing', function (t) {
-  t.plan(2)
+  t.plan(5)
 
-  console.log('\n\n\n\n\n===========')
-  console.log(store)
-
+  // ======================== //
+  // First test, store exists //
+  // ======================== //
   t.ok(store, 'Store exists')
+
+  // ===================================== //
+  // Second test, dispatch action placeAdd //
+  // ===================================== //
   store.dispatch(actions.placeAdd(
     'casa do Pai Tomás',
     -23.73454,
     -47.987
   ))
 
-  console.log('\n\n\n\n\n===========')
-  console.log(store.getState())
-  t.ok(store, 'Must change this test')
+  const firstActionResult = Immutable.fromJS({
+    observations: [],
+    events: [],
+    places: [{
+      text: 'casa do Pai Tomás',
+      lat: -23.73454,
+      lng: -47.987
+    }]
+  })
+  t.ok(Immutable.is(store.getState(), firstActionResult), 'First place added')
+
+  // ======================== //
+  // dispatch action eventAdd //
+  // ======================== //
+  store.dispatch(actions.eventAdd(
+    'Burning Man Festival 2016',
+    -23.73454,
+    -47.987
+  ))
+  const secondActionResult = firstActionResult.updateIn(['events'], obs => obs.push(Immutable.Map({
+    text: 'Burning Man Festival 2016',
+    lat: -23.73454,
+    lng: -47.987
+  })))
+
+  t.ok(Immutable.is(store.getState(), secondActionResult), 'First event added')
+
+  // ============================== //
+  // dispatch action observationAdd //
+  // ============================== //
+  store.dispatch(actions.observationAdd(
+    'The Festival is crazy, yo!',
+    -23.73454,
+    -47.987
+  ))
+  const thirdActionResult = secondActionResult.updateIn(['observations'], obs => obs.push(Immutable.Map({
+    text: 'The Festival is crazy, yo!',
+    lat: -23.73454,
+    lng: -47.987
+  })))
+
+  t.ok(Immutable.is(store.getState(), thirdActionResult), 'First observation added')
+
+  // ======================================================================= //
+  // Dispatch a second observation, because we need to see what happens when //
+  // there are several actions in one store
+  // ======================================================================= //
+  store.dispatch(actions.observationAdd(
+    'I am yet to see somethin as crazy as here',
+    -23.73454,
+    -47.987
+  ))
+  const fourthActionResult = thirdActionResult.updateIn(['observations'], obs => obs.push(Immutable.Map({
+    text: 'I am yet to see somethin as crazy as here',
+    lat: -23.73454,
+    lng: -47.987
+  })))
+  t.ok(Immutable.is(store.getState(), fourthActionResult), 'second observation added')
 })
