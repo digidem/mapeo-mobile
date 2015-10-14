657b432130cff96043232baef384677cb1e45c49
Tiago Fassoni <tiagofassoni@gmail.com>
2015-10-14 15:24:03 -0300

    Added store logger for development mode
    It is wrapped in an if, so only gets activated in development mode

--- redux/store.js
+++ redux/store.js
@@ -3,26 +3,17 @@ import { combineReducers } from 'redux-immutablejs'
 import thunk from 'redux-thunk'
 import promise from 'redux-promise'
 import createLogger from 'redux-logger'
-
 import * as reducers from './reducers/reducers'
-import Immutable from 'immutable'
 
-console.log('\n\n\n\n\n===========')
-console.log('Vai entrar no combineReducers')
 const reducer = combineReducers(reducers)
-console.log('Passou do combineReducers')
-const state = Immutable.fromJS({})
 
 // If we are developing, we apply logger middleware
 let createStoreWithMiddleware
 if (process.env.NODE_ENV === 'development') {
-  const logger = createLogger()
+  const logger = createLogger({ logger: logger || console })
   createStoreWithMiddleware = applyMiddleware(thunk, promise, logger)(createStore)
 } else {
   createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
 }
-console.log('Vai criar a store')
-//const store = createStoreWithMiddleware(reducer)
-const store = createStore(reducer)
-console.log('Passou do applyMiddleware')
+const store = createStoreWithMiddleware(reducer)
 export default store
