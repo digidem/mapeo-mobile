889050d71a955d03206f336ded1f0d96de182d46
Tiago Fassoni <tiagofassoni@gmail.com>
2015-11-02 11:39:24 -0200

    Adds Redux Router integration

--- redux/store.js
+++ redux/store.js
@@ -1,19 +1,35 @@
-import { createStore, applyMiddleware } from 'redux'
-import { combineReducers } from 'redux-immutablejs'
-import thunk from 'redux-thunk'
-import promise from 'redux-promise'
-import createLogger from 'redux-logger'
+import { reduxReactRouter, routerStateReducer } from 'redux-router'
+import { createHistory } from 'history'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+// For tap events to work with Material UI
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
+
+// import { combineReducers } from 'redux-immutablejs'
+import { devTools } from 'redux-devtools'
+// import thunk from 'redux-thunk'
+// import promise from 'redux-promise'
+// import createLogger from 'redux-logger'
 import * as reducers from './reducers/reducers'
 
-const reducer = combineReducers(reducers)
+const reducer = combineReducers({
+  router: routerStateReducer,
+  ...reducers})
 
 // If we are developing, we apply logger middleware
-let createStoreWithMiddleware
-if (process.env.NODE_ENV === 'development') {
-  const logger = createLogger({ logger: logger || console })
-  createStoreWithMiddleware = applyMiddleware(thunk, promise, logger)(createStore)
-} else {
-  createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
-}
-const store = createStoreWithMiddleware(reducer)
+// let createStoreWithMiddleware
+// let store
+// if (process.env.NODE_ENV === 'development') {
+  // const logger = createLogger({ logger: logger || console })
+  const store = compose(
+    reduxReactRouter({createHistory}),
+    devTools()
+  )(createStore)(reducer)
+// } else {
+//   store = compose(
+//     applyMiddleware(thunk),
+//     reduxReactRouter({createHistory}),
+//   )(createStore)(reducer)
+// }
+// const store = createStoreWithMiddleware(reducer)
 export default store
