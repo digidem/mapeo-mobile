3355688db743250627f3a361e26ad6d2d4faf5b5
Gregor MacLennan <gmaclennan@digital-democracy.org>
2015-11-13 16:10:34 -0700

    Cleanup some errors / add some checks

--- src/components/geolocation.jsx
+++ src/components/geolocation.jsx
@@ -1,28 +1,32 @@
 import { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { geolocationUpdate } from '../action_creators'
-import { geolocationErrors } from '../action_creators'
 
 class Geolocation extends Component {
 
   watchID = null
 
   componentDidMount () {
-    // This check is just a precaution to throw an error if geolocation is not available.
-    // Hopefully it will be never needed.
-    if ( !('geolocation' in navigator) ) { new Error() }
+    // Dispatch error and return early if we don't have geolocation
+    if (!('geolocation' in navigator)) {
+      const error = new Error('Geolocation not supported by client')
+      this.props.dispatch(geolocationUpdate(error, true))
+      return
+    }
     // The watchPosition constantly updates the store with new values (once at 300 miliseconds),
     // according to my testing. Some of the values in 'meta' positions can be NaN or may not exist
     // at all, depending on the implementation. So I'm adding checks here.
     this.watchID = navigator.geolocation.watchPosition(
       (position) => this.props.dispatch(geolocationUpdate({ position })),
-      (error) => this.props.dispatch(geolocationUpdate({ error })),
+      (error) => this.props.dispatch(geolocationUpdate(error, true)),
       {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
     )
   }
 
   componentWillUnmount () {
-    navigator.geolocation.clearWatch(this.watchID)
+    if (this.watchID) {
+      navigator.geolocation.clearWatch(this.watchID)
+    }
   }
 
   render () {
@@ -34,4 +38,5 @@ Geolocation.propTypes = {
   dispatch: PropTypes.function
 }
 
+// Connect with no arguments injects the dispatch function into props
 export default connect()(Geolocation)
