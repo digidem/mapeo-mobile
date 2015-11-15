568d32a88276d4bceb75942c439e3d967124fb10
Gregor MacLennan <gmaclennan@digital-democracy.org>
2015-11-15 09:25:21 -0700

    Make AddButton pure render

--- src/components/add_button.jsx
+++ src/components/add_button.jsx
@@ -6,10 +6,13 @@ import AddIcon from 'material-ui/lib/svg-icons/content/add'
 import HelpIcon from 'material-ui/lib/svg-icons/action/help'
 import IconButton from 'material-ui/lib/icon-button'
 import Colors from 'material-ui/lib/styles/colors'
+import shouldPureComponentUpdate from 'react-pure-render/function'
 
 const styles = {
   wrapper: {
-    marginBottom: 15
+    marginBottom: 15,
+    zIndex: 1,
+    position: 'relative'
   },
   listItem: {
     height: 72,
@@ -20,30 +23,38 @@ const styles = {
   }
 }
 
-const AddButton = ({ onTouchTap }) => (
-  <Paper
-    rounded={false}
-    style={styles.wrapper}
-    zIndex={1}
-  >
-    <ListItem
-      style={styles.listItem}
-      leftAvatar={<Avatar
-        backgroundColor={Colors.red500}
-        icon={<AddIcon />}
-      />}
-      rightIconButton={<IconButton
-        tooltip={'Help'} touch>
-        <HelpIcon color={Colors.grey400}/>
-      </IconButton>}
-      primaryText={
-        <div style={styles.text}>I see...</div>
-      }
-      secondaryText={' '}
-      onTouchTap={onTouchTap}
-    />
-  </Paper>
-)
+class AddButton extends React.Component {
+  shouldComponentUpdate = shouldPureComponentUpdate
+
+  render () {
+    const { onTouchTap } = this.props
+    return (
+      <Paper
+        rounded={false}
+        style={styles.wrapper}
+        zIndex={1}
+      >
+        <ListItem
+          style={styles.listItem}
+          leftAvatar={<Avatar
+            backgroundColor={Colors.red500}
+            icon={<AddIcon />}
+          />}
+          rightIconButton={<IconButton
+            tooltip={'Help'} touch>
+            <HelpIcon color={Colors.grey400}/>
+          </IconButton>}
+          primaryText={
+            <div style={styles.text}>I see...</div>
+          }
+          secondaryText={' '}
+          onTouchTap={onTouchTap}
+          disableTouchRipple
+        />
+      </Paper>
+    )
+  }
+}
 
 AddButton.propTypes = {
   onTouchTap: PropTypes.func
