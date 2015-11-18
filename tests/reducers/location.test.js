69ff2bd3982c0c7df2a433e0b0355c15ba359fc0
Gregor MacLennan <gmaclennan@digital-democracy.org>
2015-11-17 17:04:03 -0700

    Update location reducer tests

--- tests/reducers/location.test.js
+++ tests/reducers/location.test.js
@@ -4,95 +4,102 @@ import { geolocationUpdate } from '../../src/action_creators'
 import { geolocationErrors } from '../../src/constants'
 import PositionError from '../mock-errors.js'
 
-test('Empty reducer', (t) => {
-  const shouldBeState = {
-    coords: [-59.5, 2.7],
-    meta: {
-      accuracy: null,
-      altitude: null,
-      altitudeAccuracy: null,
-      heading: null,
-      speed: null
-    },
-    positionError: geolocationErrors.POSITION_UNAVAILABLE,
-    timestamp: null
-  }
+const emptyMeta = {
+  accuracy: null,
+  altitude: null,
+  altitudeAccuracy: null,
+  heading: null,
+  speed: null
+}
 
-  const resultingState = locationReducer(undefined, {type: 'whatever'})
-  t.deepEqual(resultingState, shouldBeState, 'Empty reducer okay')
+test('Default state', (t) => {
+  const defaultState = locationReducer(undefined, {type: 'whatever'})
+
+  t.equal(defaultState.coords, null, 'Coords null')
+  t.deepEqual(defaultState.meta, emptyMeta, 'Default meta object does not have any values')
+  t.true(defaultState.error instanceof Error, 'Defaults to location error')
+  t.equal(defaultState.error.code, geolocationErrors.POSITION_UNAVAILABLE, 'Default error type is "POSITION_UNAVAILABLE"')
+  t.equal(defaultState.timestamp, null, 'Timestamp null')
   t.end()
 })
 
-test('Reducer handles Error()', (t) => {
-  const shouldBeState = {
-    coords: [-59.5, 2.7],
-    meta: {
-      accuracy: null,
-      altitude: null,
-      altitudeAccuracy: null,
-      heading: null,
-      speed: null
-    },
-    positionError: geolocationErrors.POSITION_UNAVAILABLE,
-    timestamp: null
-  }
-
-  const resultingState = locationReducer(undefined, geolocationUpdate(new Error(), true))
-
-  t.deepEqual(resultingState, shouldBeState, 'Reducer handles Error() okay')
+test('Ignores actions when type is not "GEOLOCATION_UPDATE"', (t) => {
+  const state = {}
+  const newState = locationReducer(state, {type: 'whatever'})
+  t.equal(state, newState, 'state is unchanged by action')
   t.end()
 })
 
-test('Reducer handles geolocation Error', (t) => {
-  const MockError = new PositionError()
-
-  const resultingState = locationReducer(undefined, geolocationUpdate(MockError, true))
-
-  const shouldBeState = {
+test('Error handling - no error code', (t) => {
+  const initialState = {
     coords: [-59.5, 2.7],
-    meta: {
-      accuracy: null,
-      altitude: null,
-      altitudeAccuracy: null,
-      heading: null,
-      speed: null
-    },
-    positionError: geolocationErrors.PERMISSION_DENIED,
-    timestamp: null
-  }
-
-  t.deepEqual(resultingState, shouldBeState, 'Reducer handles geolocation Error okay')
-  t.end()
-})
-
-test('Reducer handles geolocation update', (t) => {
-  const shouldBeState = {
-    coords: [0, 0],
-    meta: {
-      accuracy: 20,
-      altitude: 700,
-      altitudeAccuracy: 50
-    },
+    meta: emptyMeta,
     positionError: null,
-    timestamp: new Date(1)
+    timestamp: 1447803656154
   }
+
+  const newState = locationReducer(initialState, geolocationUpdate(new Error(), true))
+
+  t.equal(initialState.coords, newState.coords, 'Coordinates are not changed')
+  t.equal(initialState.meta, newState.meta, 'meta is not changed')
+  t.equal(initialState.timestamp, newState.timestamp, 'timestamp is not changed')
+  t.true(newState.positionError instanceof Error, 'positionError is instance of Error')
+  t.equal(newState.positionError.code, geolocationErrors.POSITION_UNAVAILABLE, 'positionError default code is "POSITION_UNAVAILABLE"')
+  t.end()
+})
+
+test('Error handling - geolocation position denied Error', (t) => {
+  const positionDeniedError = new Error('Geolocation permission denied')
+  positionDeniedError.code = geolocationErrors.PERMISSION_DENIED
+
+  const initialState = {
+    coords: [-59.5, 2.7],
+    meta: emptyMeta,
+    positionError: null,
+    timestamp: 1447803656154
+  }
+
+  const newState = locationReducer(initialState, geolocationUpdate(positionDeniedError, true))
+  t.equal(initialState.coords, newState.coords, 'Coordinates are not changed')
+  t.equal(initialState.meta, newState.meta, 'meta is not changed')
+  t.equal(initialState.timestamp, newState.timestamp, 'timestamp is not changed')
+  t.true(newState.positionError instanceof Error, 'positionError is changed to Error')
+  t.equal(newState.positionError.code, positionDeniedError.code, 'positionError default code is "POSITION_UNAVAILABLE"')
+  t.end()
+})
+
+test('Geolocation update updates state', (t) => {
+  const initialState = {
+    coords: [-59.5, 2.7],
+    meta: emptyMeta,
+    positionError: new Error(),
+    timestamp: 1447803656154
+  }
+
   const payload = {
     position: {
       coords: {longitude: 0, latitude: 0, accuracy: 20, altitude: 700, altitudeAccuracy: 50},
       timestamp: new Date(1)
     }}
 
-  const resultingState = locationReducer(undefined, geolocationUpdate(payload))
-  t.deepEqual(resultingState, shouldBeState, 'Reducer handles geolocation update okay')
+  const {longitude, latitude, ...expectedMeta} = payload.position.coords
+  const expectedCoords = [longitude, latitude]
+
+  const newState = locationReducer(initialState, geolocationUpdate(payload))
+
+  t.deepEqual(newState.coords, expectedCoords, 'Coords are updated to new coords')
+  t.deepEqual(newState.meta, expectedMeta, 'Meta is updated to new meta')
+  t.equal(newState.positionError, null, 'positionError is set to null')
+  t.equal(newState.timestamp, payload.position.timestamp, 'Timestamp is updated to timestamp of new position')
   t.end()
 })
 
 test('Let\'s break some invariants!', (t) => {
-  t.throws(locationReducer.bind(this, undefined, geolocationUpdate({'whatever': 'yo'}, true)), 'error is true and payload is not an Error()')
+  t.throws(locationReducer.bind(this, undefined, geolocationUpdate({'whatever': 'yo'}, true)), 'Throws if error is true and payload is not an Error()')
 
-  t.throws(locationReducer.bind(this, undefined, geolocationUpdate(new Error(), false)), 'error is true and payload is a damn Error()')
+  t.throws(locationReducer.bind(this, undefined, geolocationUpdate(new Error(), false)), 'Throws if error is false and payload is a damn Error()')
 
-  t.throws(locationReducer.bind(this, undefined, geolocationUpdate({'whatever': 'yo'}, false)), 'error is false but there is no position anywhere to be seen')
+  t.throws(locationReducer.bind(this, undefined, geolocationUpdate({'whatever': 'yo'}, false)), 'Throws if error is false but there is no position anywhere to be seen')
 
   t.end()
 })
