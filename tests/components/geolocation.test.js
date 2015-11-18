b2f00f90d38a686056b83a27706c86c2ba394b97
Gregor MacLennan <gmaclennan@digital-democracy.org>
2015-11-17 17:19:36 -0700

    Only test component lifecycle (mount, unmount) once

--- tests/components/geolocation.test.js
+++ tests/components/geolocation.test.js
@@ -20,20 +20,27 @@ const stubs = {
 
 const Geolocation = proxyquire('../../src/components/geolocation', stubs)
 
+test('Component lifecycle', function (t) {
+  const dispatch = sinon.spy()
+  const shallowRenderer = createRenderer()
+
+  shallowRenderer.render(<Geolocation dispatch={dispatch} />)
+  t.false(shallowRenderer.getRenderOutput(), 'does not render anything')
+  shallowRenderer.unmount()
+  t.ok(geolocationMock.areWatchesRemoved(), 'watches are removed after component unmounts')
+  t.end()
+})
+
 test('Geolocation error position', function (t) {
   const dispatch = sinon.spy()
   const shallowRenderer = createRenderer()
   const mockError = new Error()
 
   shallowRenderer.render(<Geolocation dispatch={dispatch} />)
-  t.false(shallowRenderer.getRenderOutput(), 'does not render anything')
   geolocationMock.sendError(mockError)
   t.equal(dispatch.args[0][0].type, actionTypes.GEOLOCATION_UPDATE, 'action type is correct')
   t.equal(dispatch.args[0][0].payload, mockError, 'payload is the Error returned from geolocation')
   t.true(dispatch.args[0][0].error, 'error variable is true')
-  // Add checks for dispatches arguments...
-  shallowRenderer.unmount()
-  t.ok(geolocationMock.areWatchesRemoved(), 'watches are removed after component unmounts')
   t.end()
 })
 
@@ -42,12 +49,10 @@ test('Geolocation good position', function (t) {
   const shallowRenderer = createRenderer()
 
   shallowRenderer.render(<Geolocation dispatch={dispatch} />)
-  t.false(shallowRenderer.getRenderOutput(), 'does not render anything')
   geolocationMock.sendPosition({ location: [1, 2] })
   t.equal(dispatch.args[0][0].type, actionTypes.GEOLOCATION_UPDATE, 'action type is correct')
   t.deepEqual(dispatch.args[0][0].payload, {position: { location: [1, 2] }}, 'passes expected payload')
-  shallowRenderer.unmount()
-  t.ok(geolocationMock.areWatchesRemoved(), 'watches are removed after component unmounts')
+  t.false(shallowRenderer.getRenderOutput(), 'dispatching update does not render anything')
   t.end()
 })
 
@@ -56,15 +61,14 @@ test('Geolocation handles updates on location', function (t) {
   const shallowRenderer = createRenderer()
 
   shallowRenderer.render(<Geolocation dispatch={dispatch} />)
-  t.false(shallowRenderer.getRenderOutput(), 'does not render anything')
   geolocationMock.sendPosition({ location: [1, 2] })
+  geolocationMock.sendPosition({ location: [3, 4] })
+  geolocationMock.sendPosition({ location: [5, 6] })
   t.equal(dispatch.args[0][0].type, actionTypes.GEOLOCATION_UPDATE, 'action type is correct')
   t.deepEqual(dispatch.args[0][0].payload, {position: { location: [1, 2] }}, 'passes expected payload')
-  geolocationMock.sendPosition({ location: [3, 4] })
   t.deepEqual(dispatch.args[1][0].payload, {position: { location: [3, 4] }}, 'passes second payload')
-  geolocationMock.sendPosition({ location: [5, 6] })
   t.deepEqual(dispatch.args[2][0].payload, {position: { location: [5, 6] }}, 'passes third payload')
-  shallowRenderer.unmount()
-  t.ok(geolocationMock.areWatchesRemoved(), 'watches are removed after component unmounts')
+  t.equal(dispatch.callCount, 3, 'dispatch is called 3 times, once for each update')
+  t.false(shallowRenderer.getRenderOutput(), 'dispatching multiple updates does not render anything')
   t.end()
 })
