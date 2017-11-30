9893d259e4b961cd744c39b9c3927f0ff8c125a2
Andre Staltz <andre@staltz.com>
2017-11-30 15:30:21 +0200

    Add travis CI configuration

--- build-rnnodeapp.sh
+++ build-rnnodeapp.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Setting up...";
 rm -rf ./rnnodeapp;
--- run-benchmark.sh
+++ run-benchmark.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 # Build the background Node.js process containing only tests
 ./build-rnnodeapp.sh benchmark && \
   # Clean the React Native JavaScript app
-  watchman watch-del-all && rm -rf $TMPDIR/react-* && \
+  if [ -x "$(command -v watchman)" ]; then watchman watch-del-all && rm -rf $TMPDIR/react-*; fi && \
   # Build the React Native Android app for benchmarking
   cd android && ./gradlew clean && ./gradlew assembleBenchmarkDebug && cd .. && \
   # Run automated test that verifies the benchmark results
