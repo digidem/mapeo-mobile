026fbf6b93239394e2765f9d8e2098cac7c957da
Andre Staltz <andre@staltz.com>
2017-12-05 19:40:00 +0200

    Improve benchmark script to start/stop appium server

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
+set -e
+
+#
+# Note: this script is intended to run on a development computer,
+# not in continuous integration (Travis)
+#
 
 # Build the background Node.js process containing only tests
-./build-rnnodeapp.sh benchmark && \
-  # Clean the React Native JavaScript app
-  if [ -x "$(command -v watchman)" ]; then watchman watch-del-all && rm -rf $TMPDIR/react-*; fi && \
-  # Build the React Native Android app for benchmarking
-  cd android && ./gradlew clean && ./gradlew assembleBenchmarkDebug && cd .. && \
-  # Run automated test that verifies the benchmark results
-  node ./benchmark/test.js;
+./build-rnnodeapp.sh benchmark
+
+# Clean the React Native JavaScript app
+watchman watch-del-all && rm -rf $TMPDIR/react-*
+
+# Build the React Native Android app for benchmarking
+cd android && ./gradlew clean && ./gradlew assembleBenchmarkDebug && cd ..
+
+# Start Appium server
+$(npm bin)/appium > appium.log & echo $! > appium-server.pid
+sleep 8
+
+# Run automated test that verifies the benchmark results
+node ./benchmark/test.js
+
+# Stop Appium server
+if ps -p $(cat appium-server.pid)> /dev/null; then
+  kill -KILL $(cat appium-server.pid);
+fi
+rm appium-server.pid
+rm appium.log
