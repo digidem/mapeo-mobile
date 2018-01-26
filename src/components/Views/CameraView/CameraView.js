e6655482a264d5752bfe945253f2ea2bf5b0b2d2
Helena Qin <helena.qin@gmail.com>
2018-01-25 17:22:25 -0800

    Clean-up of initial navigation

--- src/components/Views/CameraView/CameraView.js
+++ src/components/Views/CameraView/CameraView.js
@@ -3,48 +3,6 @@ import React from 'react';
 import { Image, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 import { DrawerNavigator } from 'react-navigation';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-// import MyObservationsView from '@src/components/Views/MyObservationsView/MyObservationsView';
-
-// type State = {
-//   isRecording: boolean,
-// };
-
-// export type StateProps = {
-//   observations: {
-//     [id: string]: Observation,
-//   },
-// };
-
-// export type DispatchProps = {
-//   listObservations: () => void,
-// };
-
-// const styles = StyleSheet.create({
-// });
-
-// class CameraView extends React.PureComponent<StateProps & DispatchProps, State> {
-//   state = { isRecording: false };
-
-//   render() {
-//     return (
-//       <View style={{flex: 1}}>
-//         <Text>CameraView</Text>
-//         <Button
-//           onPress={() => navigation.navigate('DrawerOpen')}
-//           title='My Observations'
-//         />
-//       </View>
-//     );
-//   }
-// }
-
-// export default CameraView;
-
-const styles = StyleSheet.create({
-  myObservationsIcon: {
-
-  }
-});
 
 const CameraNavView = ({ navigation }) => (
   <View style={{flex: 1, alignItems: 'flex-end'}}>
