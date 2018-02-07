64e4689db5d0690f51c3c2d745bd7ef12c2e061c
Helena Qin <helena.qin@gmail.com>
2018-02-06 16:55:11 -0800

    Clean-up

--- src/ducks/index.js
+++ src/ducks/index.js
@@ -1,16 +1,12 @@
 import { combineReducers } from 'redux';
 import observations from './observations';
 
-import MainDrawerNavigation from '@src/components/MainNavigation/MainDrawerNavigation';
 import MainStackNavigation from '@src/components/MainNavigation/MainStackNavigation';
 import TabBarNavigation from '@src/components/Views/TabBarView/TabBarNavigation';
 
 const rootReducer = combineReducers({
   ...observations,
 
-  mainDrawer: (state, action) =>
-    MainDrawerNavigation.router.getStateForAction(action, state),
-  
   mainStack: (state, action) =>
     MainStackNavigation.router.getStateForAction(action, state),
 
