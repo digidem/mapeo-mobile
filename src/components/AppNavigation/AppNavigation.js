f0aa671d5f309be71991fe7f40cedee694604bca
Cindy Zeng <czeng67@gmail.com>
2018-02-19 18:09:11 -0800

    Hook up Android back button

--- src/components/AppNavigation/AppNavigation.js
+++ src/components/AppNavigation/AppNavigation.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
-import { addNavigationHelpers } from 'react-navigation';
+import { BackHandler } from 'react-native';
+import { addNavigationHelpers, NavigationActions } from 'react-navigation';
 import { createReduxBoundAddListener } from 'react-navigation-redux-helpers';
 import MainStackNavigation from '@src/components/MainNavigation/MainStackNavigation';
 
@@ -9,14 +10,33 @@ interface Props {
   navigationState: any;
 }
 
-const AppNavigation = (props: Props) => (
-  <MainStackNavigation
-    navigation={addNavigationHelpers({
-      dispatch: props.dispatch,
-      state: props.navigationState,
-      addListener: createReduxBoundAddListener('mainStack')
-    })}
-  />
-);
+class AppNavigation extends React.PureComponent<Props> {
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      const { dispatch, navigationState } = this.props;
+      if (this.shouldCloseApp(navigationState)) return false;
+      dispatch(NavigationActions.back());
+      return true;
+    });
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress');
+  }
+
+  shouldCloseApp = (nav: any) => nav.index === 0;
+
+  render() {
+    return (
+      <MainStackNavigation
+        navigation={addNavigationHelpers({
+          dispatch: this.props.dispatch,
+          state: this.props.navigationState,
+          addListener: createReduxBoundAddListener('mainStack')
+        })}
+      />
+    );
+  }
+}
 
 export default AppNavigation;
