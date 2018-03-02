2c8bbd5dee8a3fa360eacddbf08b5cf20e084612
Helena Qin <helena.qin@gmail.com>
2018-03-01 23:24:28 -0800

    Replace camera tab with my observations

--- src/components/Views/TabBarView/TabBarNavigation.js
+++ src/components/Views/TabBarView/TabBarNavigation.js
@@ -2,14 +2,9 @@
 import React from 'react';
 import { addNavigationHelpers } from 'react-navigation';
 import { StyleSheet, TouchableHighlight, View } from 'react-native';
-import Drawer from 'react-native-drawer';
 import { createReduxBoundAddListener } from 'react-navigation-redux-helpers';
-import PreferencesView from '@src/components/Views/PreferencesView/PreferencesView';
 import MyObservationsView from '@src/components/Views/MyObservationsView';
 import { WHITE } from '@lib/styles';
-
-import ProfileImg from 'react-native-vector-icons/FontAwesome';
-import CollectionsImg from 'react-native-vector-icons/MaterialIcons';
 
 import TabBar from './TabBar';
 
@@ -34,87 +29,18 @@ export type StateProps = {
 
 class TabBarNavigation extends React.Component<StateProps> {
   static router = TabBar.router;
-  leftDrawer: Drawer;
-  rightDrawer: Drawer;
-
-  closeLeftDrawer = () => {
-    this.leftDrawer.close();
-  };
-
-  openLeftDrawer = () => {
-    this.leftDrawer.open();
-  };
-
-  closeRightDrawer = () => {
-    this.rightDrawer.close();
-  };
-
-  openRightDrawer = () => {
-    this.rightDrawer.open();
-  };
-
-  handleLeftDrawerRef = (ref: Drawer) => {
-    this.leftDrawer = ref;
-  };
-
-  handleRightDrawerRef = (ref: Drawer) => {
-    this.rightDrawer = ref;
-  };
 
   render() {
     const { dispatch, navigationState } = this.props;
 
     return (
-      <Drawer
-        ref={this.handleLeftDrawerRef}
-        content={<PreferencesView closeLeftDrawer={this.closeLeftDrawer} />}
-        openDrawerOffset={0}
-        type="displace"
-      >
-        <Drawer
-          ref={this.handleRightDrawerRef}
-          content={
-            <MyObservationsView closeRightDrawer={this.closeRightDrawer} />
-          }
-          openDrawerOffset={0}
-          side="right"
-          type="displace"
-        >
-          <View
-            style={{
-              flexDirection: 'row',
-              height: 60,
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              right: 0,
-              zIndex: 5
-            }}
-          >
-            <TouchableHighlight
-              onPress={this.openLeftDrawer}
-              style={styles.profileIcon}
-              underlayColor="transparent"
-            >
-              <ProfileImg color={WHITE} name="user-circle" size={40} />
-            </TouchableHighlight>
-            <TouchableHighlight
-              onPress={this.openRightDrawer}
-              style={styles.myObservationsIcon}
-              underlayColor="transparent"
-            >
-              <CollectionsImg color={WHITE} name="collections" size={40} />
-            </TouchableHighlight>
-          </View>
-          <TabBar
-            navigation={addNavigationHelpers({
-              dispatch,
-              state: navigationState,
-              addListener: createReduxBoundAddListener('tabBar')
-            })}
-          />
-        </Drawer>
-      </Drawer>
+      <TabBar
+        navigation={addNavigationHelpers({
+          dispatch,
+          state: navigationState,
+          addListener: createReduxBoundAddListener('tabBar')
+        })}
+      />
     );
   }
 }
