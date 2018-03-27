5824ca70f708632a750df96266ccb42346c03dc5
Cindy Zeng <czeng67@gmail.com>
2018-03-27 23:55:14 +0900

    Fix for MyObservations crash

--- src/epics/observations.js
+++ src/epics/observations.js
@@ -45,176 +45,176 @@ export const observationListEpic = (
             />
           ),
           mock: true
+        },
+        {
+          type: 'Hunting site',
+          id: 2,
+          lat: 0.088170932,
+          lon: -77.44020885,
+          link: 'link',
+          created: new Date('2018-03-25T12:30:00Z'),
+          name: 'Hunting site',
+          notes: `Don Pancho's hunting camp`,
+          observedBy: 'Aliya',
+          media: [
+            {
+              type: 'LocalPhoto',
+              source: require('../images/mock-data/4-camp.jpg')
+            }
+          ],
+          icon: (
+            <Image
+              source={require('../images/categories/category_2.png')}
+              style={{ width: 25, height: 25 }}
+              resizeMode="contain"
+            />
+          ),
+          mock: true
+        },
+        {
+          type: 'Threat',
+          id: 3,
+          lat: 0.112411067,
+          lon: -77.41378756,
+          link: 'link',
+          created: new Date('2018-02-03T17:30:00Z'),
+          name: 'Threat',
+          notes: `Oil spill 3 February 2018. Ruptured pipeline. Dead fish, oily water`,
+          observedBy: 'Aliya',
+          media: [
+            {
+              type: 'LocalPhoto',
+              source: require('../images/mock-data/5-threat.jpg')
+            }
+          ],
+          icon: null,
+          mock: true
+        },
+        {
+          type: 'Medicinal plants',
+          id: 4,
+          lat: 0.09749025,
+          lon: -77.43614404,
+          link: 'link',
+          created: new Date('2018-02-04T17:30:00Z'),
+          name: 'Medicinal plants',
+          notes: `Salola fruit plant - used in tea to heal inflamation`,
+          observedBy: 'Aliya',
+          media: [
+            {
+              type: 'LocalPhoto',
+              source: require('../images/mock-data/6-plant.jpg')
+            }
+          ],
+          icon: (
+            <Image
+              source={require('../images/categories/category_6.png')}
+              style={{ width: 25, height: 25 }}
+              resizeMode="contain"
+            />
+          ),
+          mock: true
+        },
+        {
+          type: 'Waterfalls',
+          id: 5,
+          lat: 0.105818148,
+          lon: -77.4408037,
+          link: 'link',
+          created: new Date('2018-03-04T17:30:00Z'),
+          name: 'Waterfalls',
+          notes: `Sacred waterfall, visited during vision quests for ancestors, for taking ayahuasca`,
+          observedBy: 'Aliya',
+          media: [
+            {
+              type: 'LocalPhoto',
+              source: require('../images/mock-data/7-waterfall.jpg')
+            }
+          ],
+          icon: (
+            <Image
+              source={require('../images/categories/category_13.png')}
+              style={{ width: 25, height: 25 }}
+              resizeMode="contain"
+            />
+          ),
+          mock: true
+        },
+        {
+          type: 'Rivers and streams',
+          id: 6,
+          lat: 0.119004333,
+          lon: -77.42374225,
+          link: 'link',
+          created: new Date('2018-03-24T17:30:00Z'),
+          name: 'Rivers and streams',
+          notes: `Point where pipeline crosses river`,
+          observedBy: 'Aliya',
+          media: [
+            {
+              type: 'LocalPhoto',
+              source: require('../images/mock-data/8-threat.jpg')
+            }
+          ],
+          icon: (
+            <Image
+              source={require('../images/categories/category_0.png')}
+              style={{ width: 25, height: 25 }}
+              resizeMode="contain"
+            />
+          ),
+          mock: true
+        },
+        {
+          type: 'Historical sites',
+          id: 7,
+          lat: 0.082856811,
+          lon: -77.4429836,
+          link: 'link',
+          created: new Date('2018-03-24T12:30:00Z'),
+          name: 'Historical sites',
+          notes: `There used to be a village here in the 1940s, it was abandoned after an epidemic of flu which killed most of the elders and children.`,
+          observedBy: 'Aliya',
+          media: [
+            {
+              type: 'LocalPhoto',
+              source: require('../images/mock-data/9-specialsite.jpg')
+            }
+          ],
+          icon: (
+            <Image
+              source={require('../images/categories/category_17.png')}
+              style={{ width: 25, height: 25 }}
+              resizeMode="contain"
+            />
+          ),
+          mock: true
+        },
+        {
+          type: 'Gold',
+          id: 8,
+          lat: 0.108605553,
+          lon: -77.41560494,
+          link: 'link',
+          created: new Date('2018-03-11T17:30:00Z'),
+          name: 'Gold',
+          notes: `Illegal gold miners 11 March 2018, excavators, dredges, open tailing pond, mercury.`,
+          observedBy: 'Aliya',
+          media: [
+            {
+              type: 'LocalPhoto',
+              source: require('../images/mock-data/10-threat.jpg')
+            }
+          ],
+          icon: (
+            <Image
+              source={require('../images/categories/category_8.png')}
+              style={{ width: 25, height: 25 }}
+              resizeMode="contain"
+            />
+          ),
+          mock: true
         }
-        // {
-        //   type: 'Hunting site',
-        //   id: 4,
-        //   lat: 0.088170932,
-        //   lon: -77.44020885,
-        //   link: 'link',
-        //   created: new Date('2018-03-25T12:30:00Z'),
-        //   name: 'Hunting site',
-        //   notes: `Don Pancho's hunting camp`,
-        //   observedBy: 'Aliya',
-        //   media: [
-        //     {
-        //       type: 'LocalPhoto',
-        //       source: require('../images/mock-data/4-camp.jpg')
-        //     }
-        //   ],
-        //   icon: (
-        //     <Image
-        //       source={require('../images/categories/category_2.png')}
-        //       style={{ width: 25, height: 25 }}
-        //       resizeMode="contain"
-        //     />
-        //   ),
-        //   mock: true
-        // },
-        // {
-        //   type: 'Threat',
-        //   id: 5,
-        //   lat: 0.112411067,
-        //   lon: -77.41378756,
-        //   link: 'link',
-        //   created: new Date('2018-02-03T17:30:00Z'),
-        //   name: 'Threat',
-        //   notes: `Oil spill 3 February 2018. Ruptured pipeline. Dead fish, oily water`,
-        //   observedBy: 'Aliya',
-        //   media: [
-        //     {
-        //       type: 'LocalPhoto',
-        //       source: require('../images/mock-data/5-threat.jpg')
-        //     }
-        //   ],
-        //   icon: null,
-        //   mock: true
-        // },
-        // {
-        //   type: 'Medicinal plants',
-        //   id: 6,
-        //   lat: 0.09749025,
-        //   lon: -77.43614404,
-        //   link: 'link',
-        //   created: new Date('2018-02-04T17:30:00Z'),
-        //   name: 'Medicinal plants',
-        //   notes: `Salola fruit plant - used in tea to heal inflamation`,
-        //   observedBy: 'Aliya',
-        //   media: [
-        //     {
-        //       type: 'LocalPhoto',
-        //       source: require('../images/mock-data/6-plant.jpg')
-        //     }
-        //   ],
-        //   icon: (
-        //     <Image
-        //       source={require('../images/categories/category_6.png')}
-        //       style={{ width: 25, height: 25 }}
-        //       resizeMode="contain"
-        //     />
-        //   ),
-        //   mock: true
-        // },
-        // {
-        //   type: 'Waterfalls',
-        //   id: 7,
-        //   lat: 0.105818148,
-        //   lon: -77.4408037,
-        //   link: 'link',
-        //   created: new Date('2018-03-04T17:30:00Z'),
-        //   name: 'Waterfalls',
-        //   notes: `Sacred waterfall, visited during vision quests for ancestors, for taking ayahuasca`,
-        //   observedBy: 'Aliya',
-        //   media: [
-        //     {
-        //       type: 'LocalPhoto',
-        //       source: require('../images/mock-data/7-waterfall.jpg')
-        //     }
-        //   ],
-        //   icon: (
-        //     <Image
-        //       source={require('../images/categories/category_13.png')}
-        //       style={{ width: 25, height: 25 }}
-        //       resizeMode="contain"
-        //     />
-        //   ),
-        //   mock: true
-        // },
-        // {
-        //   type: 'Rivers and streams',
-        //   id: 8,
-        //   lat: 0.119004333,
-        //   lon: -77.42374225,
-        //   link: 'link',
-        //   created: new Date('2018-03-24T17:30:00Z'),
-        //   name: 'Rivers and streams',
-        //   notes: `Point where pipeline crosses river`,
-        //   observedBy: 'Aliya',
-        //   media: [
-        //     {
-        //       type: 'LocalPhoto',
-        //       source: require('../images/mock-data/8-threat.jpg')
-        //     }
-        //   ],
-        //   icon: (
-        //     <Image
-        //       source={require('../images/categories/category_0.png')}
-        //       style={{ width: 25, height: 25 }}
-        //       resizeMode="contain"
-        //     />
-        //   ),
-        //   mock: true
-        // },
-        // {
-        //   type: 'Historical sites',
-        //   id: 9,
-        //   lat: 0.082856811,
-        //   lon: -77.4429836,
-        //   link: 'link',
-        //   created: new Date('2018-03-24T12:30:00Z'),
-        //   name: 'Historical sites',
-        //   notes: `There used to be a village here in the 1940s, it was abandoned after an epidemic of flu which killed most of the elders and children.`,
-        //   observedBy: 'Aliya',
-        //   media: [
-        //     {
-        //       type: 'LocalPhoto',
-        //       source: require('../images/mock-data/9-specialsite.jpg')
-        //     }
-        //   ],
-        //   icon: (
-        //     <Image
-        //       source={require('../images/categories/category_17.png')}
-        //       style={{ width: 25, height: 25 }}
-        //       resizeMode="contain"
-        //     />
-        //   ),
-        //   mock: true
-        // },
-        // {
-        //   type: 'Gold',
-        //   id: 10,
-        //   lat: 0.108605553,
-        //   lon: -77.41560494,
-        //   link: 'link',
-        //   created: new Date('2018-03-11T17:30:00Z'),
-        //   name: 'Gold',
-        //   notes: `Illegal gold miners 11 March 2018, excavators, dredges, open tailing pond, mercury.`,
-        //   observedBy: 'Aliya',
-        //   media: [
-        //     {
-        //       type: 'LocalPhoto',
-        //       source: require('../images/mock-data/10-threat.jpg')
-        //     }
-        //   ],
-        //   icon: (
-        //     <Image
-        //       source={require('../images/categories/category_8.png')}
-        //       style={{ width: 25, height: 25 }}
-        //       resizeMode="contain"
-        //     />
-        //   ),
-        //   mock: true
-        // }
       ])
     );
 
