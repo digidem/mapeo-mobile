5782443e2a69617f86d8d1b51f6c1d0b26233e53
Cindy Zeng <czeng67@gmail.com>
2018-04-28 21:07:59 -0700

    tests passing!

---
+++ src/components/Views/TabBarNavigation/SavedModal/SavedModal.test.js
@@ -0,0 +1,25 @@
+// @flow
+import React from 'react';
+import 'react-native';
+import renderer from 'react-test-renderer';
+import SavedModal from './SavedModal';
+import { createObservation } from '../../../../mocks/observations';
+
+describe('SavedModal tests', () => {
+  test('snapshots', () => {
+    const observation = createObservation();
+    const onHide = jest.fn();
+
+    const props = [{ observation }];
+
+    let tree;
+    props.forEach(p => {
+      tree = renderer
+        .create(
+          <SavedModal selectedObservation={p.observation} onHide={onHide} />
+        )
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+});
