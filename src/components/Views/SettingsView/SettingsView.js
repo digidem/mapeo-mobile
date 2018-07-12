4cbf7c56cf46f89b6b6bb4e2c1d588504d6316b8
Gregor MacLennan <gmaclennan@digital-democracy.org>
2018-07-12 18:18:56 -0500

    Always list styles when the settings view mounts (reads updated styles from the styles folder)

--- src/components/Views/SettingsView/SettingsView.js
+++ src/components/Views/SettingsView/SettingsView.js
@@ -94,11 +94,8 @@ I18n.translations = {
 
 class SettingsView extends React.Component<Props & StateProps & DispatchProps> {
   componentDidMount() {
-    const { selectedStyle, listStyles } = this.props;
-
-    if (!selectedStyle) {
-      listStyles();
-    }
+    const { listStyles } = this.props;
+    listStyles();
   }
 
   shouldComponentUpdate(nextProps: Props & StateProps & DispatchProps) {
