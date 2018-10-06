b4e97a6eb86186ca0448800e3d7b73efb168f385
Karissa McKelvey <krmckelv@gmail.com>
2018-10-05 18:46:28 -0700

    fix: migration should handle original and thumbnail paths

--- src/rnnodeapp/migrateMedia.js
+++ src/rnnodeapp/migrateMedia.js
@@ -8,23 +8,35 @@ const safe = require('safe-fs-blob-store')
 // 2. add each file to safe-fs-blob-store
 // 3. remove old file
 
-module.exports = migrateDirectory
+module.exports = function (mediaPath, cb) {
+  migrateDirectory(path.join(mediaPath, 'original'), function (err) {
+    if (err) return cb(err)
+    migrateDirectory(path.join(mediaPath, 'thumbnail'), cb)
+  })
+}
 
-function migrateDirectory (mediaPath, cb) {
-  var blobstore = safe(mediaPath)
+function migrateDirectory (dir, cb) {
+  var blobstore = safe(dir)
   var tasks = []
-  fs.readdir(mediaPath, function (err, files) {
-    if (err) throw err
-    files.forEach(function (filename) {
-      tasks.push((next) => {
-        migrateFile(filename, next)
+  fs.stat(dir, function (err, stat) {
+    if (err) {
+      if (err.code === 'ENOENT') return cb(new Error(`Directory ${dir} does not exist.`))
+      else return cb(err)
+    }
+
+    fs.readdir(dir, function (err, files) {
+      if (err) return cb(err)
+      files.forEach(function (filename) {
+        tasks.push((next) => {
+          migrateFile(filename, next)
+        })
       })
+      parallel(tasks, cb)
     })
-    parallel(tasks, cb)
   })
 
   function migrateFile (key, cb) {
-    var filename = path.join(mediaPath, key)
+    var filename = path.join(dir, key)
     fs.stat(filename, function (err, stat) {
       if (err) return cb(err)
       if (stat.isFile()) {
@@ -33,7 +45,8 @@ function migrateDirectory (mediaPath, cb) {
           fs.unlink(filename, cb)
         }
         var readStream = fs.createReadStream(filename)
-        var writeStream = blobstore.createWriteStream({key}, cleanup)
+        var keyWithPrefix = `${path.dirname(key).split('/').pop()}/${key}`
+        var writeStream = blobstore.createWriteStream({key: keyWithPrefix}, cleanup)
         readStream.pipe(writeStream)
       }
     })
--- src/rnnodeapp/tests/migrationTest.js
+++ src/rnnodeapp/tests/migrationTest.js
@@ -3,62 +3,92 @@ const mkdirp = require('mkdirp')
 const rimraf = require('rimraf')
 const blobstore = require('fs-blob-store')
 const fs = require('fs')
+const series = require('run-series')
 const safeBlobstore = require('safe-fs-blob-store')
 const path = require('path')
 const test = require('tape')
 const migrateMedia = require('../migrateMedia')
 const os = require('os')
 
-var key = 'mapeo-icon.png'
 const TEST_IMAGES = path.join(__dirname, '..', '..', 'images')
 const MEDIA_PATH = path.join(os.tmpdir(), 'media-test')
+const ORIGINAL_PATH = path.join(MEDIA_PATH, 'original')
+const THUMBNAIL_PATH = path.join(MEDIA_PATH, 'thumbnail')
+
+function testMigration (keys, t) {
+  var old = blobstore(MEDIA_PATH)
+  var safe = safeBlobstore(MEDIA_PATH)
+
+  var tasks = []
+  keys.forEach(function (key) {
+    tasks.push((next) => {
+      var testImage = path.join(TEST_IMAGES, key)
+      writeFile(testImage, 'original/' + key, function (err) {
+        t.error(err, 'error in writing orignal for ' + key)
+        writeFile(testImage, 'thumbnail/' + key, function (err) {
+          t.error(err, 'error in writing thumbnail for ' + key)
+          fs.access(path.join(ORIGINAL_PATH, key), function (err) {
+            t.error(err, 'test image exists in media path (original)')
+            fs.access(path.join(THUMBNAIL_PATH, key), function (err) {
+              t.error(err, 'test image exists in media path (thumbnail)')
+              next()
+            })
+          })
+        })
+      })
+    })
+  })
+
+  series(tasks, function (err) {
+    t.error(err)
+    migrateMedia(MEDIA_PATH, function (err) {
+      t.error(err, 'error on migrate')
+      keys.forEach(testKey)
+    })
+  })
+
+  function writeFile (testImage, key, cb) {
+    var readStream = fs.createReadStream(testImage)
+    var writeStream = old.createWriteStream(key, cb)
+    readStream.pipe(writeStream)
+  }
+
+  let pending = keys.length
+
+  function done () {
+    pending--
+    if (pending !== 0) return
+    t.end()
+  }
+
+  function testKey (key) {
+    var newFile = safe.createReadStream('original/' + key)
+    newFile.on('error', function (err) {
+      t.error(err, 'error on reading from safe blob store')
+      t.end()
+    })
+    newFile.pipe(concat(function (data) {
+      var testImage = path.join(TEST_IMAGES, key)
+      t.same(fs.readFileSync(testImage), data)
+      fs.access(path.join(ORIGINAL_PATH, key), function (err) {
+        t.ok(err, 'test image no longer exists in old blob store format (original)')
+        fs.access(path.join(THUMBNAIL_PATH, key), function (err) {
+          t.ok(err, 'test image no longer exists in old blob store format (thumbnail)')
+          done()
+        })
+      })
+    }))
+  }
+}
 
 test('migrates files', function (t) {
   resetMediaPath()
-  var original = blobstore(MEDIA_PATH)
-  var testImage = path.join(TEST_IMAGES, key)
-
-  function done (err) {
-    t.error(err)
-    fs.access(path.join(MEDIA_PATH, key), function (err) {
-      t.ok(err, 'test image no longer exists in old blob store format')
-      t.end()
-    })
-  }
-
-  function doMigrate (err) {
-    t.error(err)
-    fs.access(path.join(MEDIA_PATH, key), function (err) {
-      t.error(err, 'test image exists in media path')
-
-      migrateMedia(MEDIA_PATH, function (err) {
-        t.error(err, 'error on migrate')
-        var safe = safeBlobstore(MEDIA_PATH)
-        console.log(key)
-        var newFile = safe.createReadStream({key})
-        newFile.on('error', function (err) {
-          t.error(err, 'error on reading from safe blob store')
-          t.end()
-        })
-        newFile.pipe(concat(function (data) {
-          t.same(fs.readFileSync(testImage), data)
-          done()
-        }))
-      })
-    })
-  }
-
-  var readStream = fs.createReadStream(testImage)
-  var writeStream = original.createWriteStream({key}, doMigrate)
-  readStream.pipe(writeStream)
+  testMigration(['mapeo-icon.png'], t)
 })
 
-test('when there is nothing to migrate, nothing happens', function (t) {
-  // resetMediaPath()
-  migrateMedia(MEDIA_PATH, function (err) {
-    t.error(err, 'error on migrate')
-    t.end()
-  })
+test('when media folder has many items', function (t) {
+  resetMediaPath()
+  testMigration(['mapeo-icon.png', 'add-button.png'], t)
 })
 
 function resetMediaPath () {
