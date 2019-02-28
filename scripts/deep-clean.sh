5eb187dbb8027d3ee0660502f846999be760a788
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-02-28 18:03:36 +0000

    Add deep clean script

---
+++ scripts/deep-clean.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Any copyright is dedicated to the Public Domain.
+# http://creativecommons.org/publicdomain/zero/1.0/
+
+set -eEu -o pipefail
+shopt -s extdebug
+IFS=$'\n\t'
+trap 'onFailure $?' ERR
+
+function onFailure() {
+  echo "Unhandled script error $1 at ${BASH_SOURCE[0]}:${BASH_LINENO[0]}" >&2
+  exit 1
+}
+
+# This does a deep-clean of cache files in react-native and gradle / Android
+# Use this if you are having issues with builds not working
+
+watchman watch-del-all
+rm -rf $TMPDIR/react-native-packager-cache-*
+rm -rf $TMPDIR/metro-bundler-cache-*
+rm -rf node_modules/
+(
+  cd android
+  ./gradlew clean
+  ./gradlew cleanBuildCache
+)
+npm install
+npm start -- --reset-cache
