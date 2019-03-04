6fa6c11e6b6214e8e4e325dd82b91a66538745ff
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-03-04 09:55:00 -0800

    Add test tab view

--- src/frontend/components/AppContainer.js
+++ src/frontend/components/AppContainer.js
@@ -1,7 +1,53 @@
-import React from "react";
-import { Text, View } from "react-native";
+// import React from "react";
+// import { Text, View } from "react-native";
 import { createBottomTabNavigator, createAppContainer } from "react-navigation";
 import LocationScreen from "./LocationScreen";
+import * as React from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { TabView, SceneMap } from "react-native-tab-view";
+
+const FirstRoute = () => (
+  <View style={[styles.scene, { backgroundColor: "#ff4081" }]} />
+);
+const SecondRoute = () => (
+  <View style={[styles.scene, { backgroundColor: "#673ab7" }]} />
+);
+
+export default class TabViewExample extends React.Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: "first", title: "First" },
+      { key: "second", title: "Second" }
+    ]
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <TabView
+          tabBarPosition="bottom"
+          navigationState={this.state}
+          renderScene={SceneMap({
+            first: FirstRoute,
+            second: SecondRoute
+          })}
+          onIndexChange={index => this.setState({ index })}
+          initialLayout={{ width: Dimensions.get("window").width }}
+        />
+        <View style={{ position: "absolute", bottom: 20 }}>
+          <Text>Hello World</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  scene: {
+    flex: 1
+  }
+});
 
 class SettingsScreen extends React.Component {
   render() {
@@ -18,4 +64,4 @@ const TabNavigator = createBottomTabNavigator({
   Settings: SettingsScreen
 });
 
-export default createAppContainer(TabNavigator);
+// export default createAppContainer(TabNavigator);
