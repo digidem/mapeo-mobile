eacbcf5b6b0a62bb6b74bd709fdba28793d7ebc4
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-03-07 05:51:43 -0800

    Add react-native-fs

--- android/app/src/main/java/com/mapeomobile/MainApplication.java
+++ android/app/src/main/java/com/mapeomobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.mapeomobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -47,6 +48,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
             new ReanimatedPackage(),
           new RNGestureHandlerPackage(),
           new SplashScreenReactPackage(),
