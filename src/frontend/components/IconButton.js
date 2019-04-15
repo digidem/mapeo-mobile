0ce7a12bee2b3d12ef4f909585a6eb3a1698861b
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-04-15 09:13:14 +0100

    Fix: testID on button

--- src/frontend/components/IconButton.js
+++ src/frontend/components/IconButton.js
@@ -1,7 +1,16 @@
 // @flow
 import * as React from "react";
+import PropTypes from "prop-types";
 import { StyleSheet, View } from "react-native";
 import { TouchableNativeFeedback } from "react-native-gesture-handler";
+
+import { VERY_LIGHT_BLUE } from "../lib/styles";
+
+// Fix warning pending https://github.com/kmagiera/react-native-gesture-handler/pull/561/files
+TouchableNativeFeedback.propTypes = {
+  ...TouchableNativeFeedback.propTypes,
+  background: PropTypes.object
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -16,11 +25,16 @@ const styles = StyleSheet.create({
 type Props = {
   onPress: (SyntheticEvent<>) => void,
   style?: any,
-  children: React.Node
+  children: React.Node,
+  testID?: string
 };
 
-const IconButton = ({ onPress, style, children }: Props) => (
-  <TouchableNativeFeedback testID="IconButton" onPress={onPress}>
+const IconButton = ({ onPress, style, children, testID }: Props) => (
+  <TouchableNativeFeedback
+    testID={testID}
+    onPress={onPress}
+    background={TouchableNativeFeedback.Ripple(VERY_LIGHT_BLUE, true)}
+  >
     <View style={[styles.container, style]}>{children}</View>
   </TouchableNativeFeedback>
 );
