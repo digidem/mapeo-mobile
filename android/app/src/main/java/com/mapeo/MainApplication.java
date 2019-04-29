584948761e9484cec3c23e2235597c2a2d9c7ae4
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-04-29 10:22:50 +0100

    Add NetInfo module

--- android/app/src/main/java/com/mapeo/MainApplication.java
+++ android/app/src/main/java/com/mapeo/MainApplication.java
@@ -3,6 +3,7 @@ package com.mapeo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
@@ -52,6 +53,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new NetInfoPackage(),
             new AsyncStoragePackage(),
             new LinearGradientPackage(),
             new ImageResizerPackage(),
