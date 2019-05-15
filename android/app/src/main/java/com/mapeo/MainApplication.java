68a3136835836895190ad344d721a7f9b4d221bd
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-05-15 12:05:20 +0100

    chore(deps): Update react-native-unimodules and expo-camera and expo-location

--- android/app/src/main/java/com/mapeo/MainApplication.java
+++ android/app/src/main/java/com/mapeo/MainApplication.java
@@ -21,30 +21,21 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import expo.adapters.react.ModuleRegistryAdapter;
-import expo.adapters.react.ReactAdapterPackage;
-import expo.adapters.react.ReactModuleRegistryProvider;
-import expo.core.interfaces.Package;
-import expo.core.interfaces.SingletonModule;
-import expo.modules.constants.ConstantsPackage;
-import expo.modules.filesystem.FileSystemPackage;
-import expo.modules.permissions.PermissionsPackage;
-import expo.modules.location.LocationPackage;
+import com.mapeo.generated.BasePackageList;
+import org.unimodules.adapters.react.ModuleRegistryAdapter;
+import org.unimodules.adapters.react.ReactModuleRegistryProvider;
+import org.unimodules.core.interfaces.SingletonModule;
+
 import com.mapbox.rctmgl.RCTMGLPackage;
-import expo.modules.camera.CameraPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
-  private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(Arrays.<Package>asList(
-    new ReactAdapterPackage(),
-    new ConstantsPackage(),
-    new PermissionsPackage(),
-    new FileSystemPackage(),
-    new LocationPackage(),
-    new CameraPackage()
-  ), Arrays.<SingletonModule>asList());
+  private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(
+    new BasePackageList().getPackageList(),
+    Arrays.<SingletonModule>asList()
+  );
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
---
+++ android/app/src/main/java/com/mapeo/generated/BasePackageList.java
@@ -0,0 +1,18 @@
+package com.mapeo.generated;
+
+import java.util.Arrays;
+import java.util.List;
+import org.unimodules.core.interfaces.Package;
+
+public class BasePackageList {
+  public List<Package> getPackageList() {
+    return Arrays.<Package>asList(
+        new org.unimodules.adapters.react.ReactAdapterPackage(),
+        new expo.modules.camera.CameraPackage(),
+        new expo.modules.location.LocationPackage(),
+        new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.filesystem.FileSystemPackage(),
+        new expo.modules.permissions.PermissionsPackage()
+    );
+  }
+}
