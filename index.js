9070d116750fee3e32cff4920c0e346bccf091bb
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-06-12 11:37:56 +0100

    Add bugsnag

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 import { AppRegistry } from "react-native";
 import App from "./src/frontend/App";
 import { name as appName } from "./app.json";
+import { Client } from "bugsnag-react-native";
+
+const bugsnag = new Client("572d472ea9d5a9199777b88ef268da4e");
+
+bugsnag.notify(new Error("Test error"));
 
 AppRegistry.registerComponent(appName, () => App);
