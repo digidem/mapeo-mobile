19ec8b3c8ca5c02bccfae3e6d7383df93e95b452
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-06-14 16:16:39 +0100

    integrate bugsnag into error boundary

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 // @flow
+// setup bugsnag before anything else
+import "./src/frontend/lib/logger";
 import { AppRegistry } from "react-native";
 import App from "./src/frontend/App";
 import { name as appName } from "./app.json";
-import { Client } from "bugsnag-react-native";
-
-const bugsnag = new Client("572d472ea9d5a9199777b88ef268da4e");
-
-bugsnag.notify(new Error("Test error"));
 
 AppRegistry.registerComponent(appName, () => App);
