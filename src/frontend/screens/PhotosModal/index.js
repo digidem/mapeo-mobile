9429c2da30282c5faff0b9d44e140c26c64c8b54
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-08-30 09:53:19 +0100

    feat: Swipe between multiple photos for an observation 🎉 (#199)

--- src/frontend/screens/PhotosModal/index.js
+++ src/frontend/screens/PhotosModal/index.js
@@ -1,65 +1,81 @@
 // @flow
-import React from "react";
+import React, { useState } from "react";
+import { Dimensions, View, StyleSheet } from "react-native";
+import { TabView } from "react-native-tab-view";
 
+import IconButton from "../../sharedComponents/IconButton";
+import { CloseIcon } from "../../sharedComponents/icons";
 import { filterPhotosFromAttachments } from "../../lib/utils";
 import useObservation from "../../hooks/useObservation";
 import useDraftObservation from "../../hooks/useDraftObservation";
-import PhotosView from "./PhotosView";
+import PhotoView from "./PhotoView";
+import api from "../../api";
+import type { NavigationProp } from "../../types";
 
-type Props = {
-  onClose: () => void,
-  photoIndex: number
-};
-
-const DraftPhotosModal = ({ onClose, photoIndex }: Props) => {
-  const [{ value, photos }] = useDraftObservation();
-  if (!value || !photos) {
-    onClose();
-    return null;
-  }
-  return (
-    <PhotosView onClose={onClose} photos={photos} initialIndex={photoIndex} />
-  );
-};
-
-const ExistingPhotosModal = ({
-  onClose,
-  photoIndex,
-  observationId
-}: Props & { observationId: string }) => {
-  const [{ observation }] = useObservation(observationId);
-  if (!observation || !observation.value.attachments) {
-    onClose();
-    return null;
-  }
-  const photos = filterPhotosFromAttachments(observation.value.attachments);
-  return (
-    <PhotosView onClose={onClose} photos={photos} initialIndex={photoIndex} />
-  );
-};
-
-const PhotosModal = ({ navigation }: { navigation: any }) => {
+const PhotosModal = ({ navigation }: { navigation: NavigationProp }) => {
   const observationId = navigation.getParam("observationId");
-  const photoIndex = navigation.getParam("photoIndex");
   const isEditing = navigation.getParam("editing");
-
-  // If we reach this modal from the editing context, then we want to show the
-  // photos which are attached to the draft observation, which may include
-  // photos which have been added and are not yet saved, so we use the draft
-  // context, otherwise we use the observation context
-  if (isEditing) {
-    return (
-      <DraftPhotosModal photoIndex={photoIndex} onClose={navigation.pop} />
-    );
-  } else {
-    return (
-      <ExistingPhotosModal
-        photoIndex={photoIndex}
-        observationId={observationId}
-        onClose={navigation.pop}
+  const [index, setIndex] = useState(navigation.getParam("photoIndex") || 0);
+  const [{ observation }] = useObservation(observationId);
+  const [{ photos: draftPhotos }] = useDraftObservation();
+  console.log(observation && observation.value.attachments);
+  console.log(draftPhotos);
+  const routes = observation
+    ? filterPhotosFromAttachments(observation.value.attachments).map(
+        (savedPhoto, idx) => ({
+          key: idx,
+          uri: api.getMediaUrl(savedPhoto.id, "preview")
+        })
+      )
+    : draftPhotos.map((draftPhoto, idx) => ({
+        key: idx,
+        uri: draftPhoto.previewUri,
+        error: draftPhoto.error,
+        capturing: draftPhoto.capturing
+      }));
+  console.log(routes);
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <IconButton
+          onPress={() => {
+            navigation.pop();
+          }}
+        >
+          <CloseIcon color="white" />
+        </IconButton>
+      </View>
+      <TabView
+        navigationState={{ index, routes }}
+        onIndexChange={setIndex}
+        renderScene={({ route }) => {
+          let variant = "photo";
+          if (route.error) variant = "error";
+          if (route.capturing) variant = "loading";
+          return (
+            <PhotoView key={route.uri} uri={route.uri} variant={variant} />
+          );
+        }}
+        initialLayout={{ width: Dimensions.get("window").width }}
+        renderTabBar={() => null}
       />
-    );
-  }
+    </View>
+  );
 };
 
 export default PhotosModal;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black"
+  },
+  header: {
+    position: "absolute",
+    zIndex: 1,
+    height: 60,
+    marginBottom: 20,
+    flexDirection: "row",
+    alignItems: "center"
+  }
+});
