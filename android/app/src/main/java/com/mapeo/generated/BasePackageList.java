fce5183d94f38eb5f43e5c366e3e89c1721b2b85
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-10-28 14:29:58 +0000

    add missing package

--- android/app/src/main/java/com/mapeo/generated/BasePackageList.java
+++ android/app/src/main/java/com/mapeo/generated/BasePackageList.java
@@ -10,6 +10,7 @@ public class BasePackageList {
         new expo.modules.camera.CameraPackage(),
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
+        new expo.modules.localization.LocalizationPackage(),
         new expo.modules.location.LocationPackage(),
         new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.sensors.SensorsPackage()
