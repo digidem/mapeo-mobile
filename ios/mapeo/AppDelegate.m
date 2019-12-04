37a7e1e0925f799aa0ead5f5baa16c111b629984
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-12-04 22:08:52 +0000

    Update to RN 0.61.5

--- ios/mapeo/AppDelegate.m
+++ ios/mapeo/AppDelegate.m
@@ -10,7 +10,6 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#import "RNSplashScreen.h"  // react-native-splash-screen
 
 @implementation AppDelegate
 
@@ -28,7 +27,6 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  [RNSplashScreen show];  // react-native-splash-screen
   return YES;
 }
 
