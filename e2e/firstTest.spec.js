456224a8f2cfc3b424a617f47f2b8eecd80c3a0c
Gregor MacLennan <gmaclennan@digital-democracy.org>
2020-02-07 13:26:30 +0000

    WIP

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -3,12 +3,17 @@ describe("Example", () => {
     await device.reloadReactNative();
   });
 
-  it("should show main screen", async () => {
-    await expect(element(by.id("root"))).toBeVisible();
+  it("should have welcome screen", async () => {
+    await expect(element(by.id("welcome"))).toBeVisible();
   });
 
-  it("should show alert after messaging node", async () => {
-    await element(by.id("message_node_button")).tap();
-    await expect(element(by.text("From node: A message!"))).toBeVisible();
+  it("should show hello screen after tap", async () => {
+    await element(by.id("hello_button")).tap();
+    await expect(element(by.text("Hello!!!"))).toBeVisible();
+  });
+
+  it("should show world screen after tap", async () => {
+    await element(by.id("world_button")).tap();
+    await expect(element(by.text("World!!!"))).toBeVisible();
   });
 });
