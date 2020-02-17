0db2baa7896369394a27162240b5a1b0e1c5acb2
Gregor MacLennan <gmaclennan@digital-democracy.org>
2020-02-17 15:26:27 +0000

    Add AppInfo native module for accessing sourceDir

--- android/app/src/main/java/com/mapeo/MainApplication.java
+++ android/app/src/main/java/com/mapeo/MainApplication.java
@@ -20,19 +20,19 @@ import org.unimodules.core.interfaces.SingletonModule;
 
 import com.mapbox.rctmgl.RCTMGLPackage;
 
+import com.mapeo.AppInfoPackage;
+
 import java.util.Arrays;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends Application implements ShareApplication, ReactApplication {
   private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(
-    new BasePackageList().getPackageList(),
-    Arrays.<SingletonModule>asList()
-  );
+      new BasePackageList().getPackageList(), Arrays.<SingletonModule>asList());
 
   @Override
   public String getFileProviderAuthority() {
-      return BuildConfig.APPLICATION_ID + ".provider";
+    return BuildConfig.APPLICATION_ID + ".provider";
   }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -45,29 +45,12 @@ public class MainApplication extends Application implements ShareApplication, Re
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // Packages that cannot be autolinked yet can be added manually here, for
+      // example:
       // packages.add(new MyReactNativePackage());
       packages.add(new ModuleRegistryAdapter(mModuleRegistryProvider));
+      packages.add(new AppInfoPackage());
       return packages;
-      // return Arrays.<ReactPackage>asList(
-      //     new MainReactPackage(),
-      //       BugsnagReactNative.getPackage(),
-      //       new RNSharePackage(),
-      //       new KCKeepAwakePackage(),
-      //       new AndroidOpenSettingsPackage(),
-      //       new RNNetworkInfoPackage(),
-      //       new NetInfoPackage(),
-      //       new AsyncStoragePackage(),
-      //       new LinearGradientPackage(),
-      //       new ImageResizerPackage(),
-      //       new RNFSPackage(),
-      //       new ReanimatedPackage(),
-      //     new RNGestureHandlerPackage(),
-      //     new SplashScreenReactPackage(),
-      //     new RNNodeJsMobilePackage(),
-      //     new RCTMGLPackage(),
-      //     new ModuleRegistryAdapter(mModuleRegistryProvider)
-      // );
     }
 
     @Override
