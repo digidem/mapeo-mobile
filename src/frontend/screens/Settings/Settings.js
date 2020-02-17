5b0e6a14b4750aaefa511bdd95f4c6ee339e049e
Gregor MacLennan <gmaclennan@digital-democracy.org>
2020-02-17 17:17:14 +0000

    Add share sheet for sharing Mapeo APK

--- src/frontend/screens/Settings/Settings.js
+++ src/frontend/screens/Settings/Settings.js
@@ -2,8 +2,12 @@
 import React from "react";
 // import { Picker as OriginalPicker } from "@react-native-community/picker";
 import { FormattedMessage, defineMessages } from "react-intl";
-import { useNavigation } from "react-navigation-hooks";
+import { useNavigation, useFocusEffect } from "react-navigation-hooks";
+import RNFS from "react-native-fs";
+import Share from "react-native-share";
+import DeviceInfo from "react-native-device-info";
 
+import AppInfo from "../../lib/AppInfo";
 import HeaderTitle from "../../sharedComponents/HeaderTitle";
 import {
   List,
@@ -11,6 +15,7 @@ import {
   ListItemText,
   ListItemIcon
 } from "../../sharedComponents/List";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
 
 const m = defineMessages({
   settingsTitle: {
@@ -37,28 +42,95 @@ const m = defineMessages({
     id: "screens.Settings.projectConfigDesc",
     defaultMessage: "Categories, icons and questions",
     description: "Secondary text for project config settings"
+  },
+  appShare: {
+    id: "screens.Settings.appShare",
+    defaultMessage: "Share Mapeo Installer",
+    description: "Primary text for sharing the mapeo APK installer"
+  },
+  appShareDesc: {
+    id: "screens.Settings.appShareDesc",
+    defaultMessage: "Install or update Mapeo on another phone",
+    description: "Secondary text for sharing the mapeo APK installer"
   }
 });
 
+const APP_VERSION = DeviceInfo.getVersion();
+
 const Settings = () => {
   const { navigate } = useNavigation();
+  const [status, setStatus] = React.useState("idle");
+  const didNavigateAway = React.useRef(false);
+
+  const shareApk = React.useCallback(async () => {
+    const tmpDir = RNFS.DocumentDirectoryPath + "/installer";
+    const apkName = `Mapeo-v${APP_VERSION}.apk`;
+    const tmpApkPath = `${tmpDir}/${apkName}`;
+    setStatus("loading");
+    try {
+      // The apk cannot be shared with other apps from the sourceDir folder, so
+      // we need to create a copy in a place that can be shared. We delete the
+      // copy afterwards
+      await RNFS.mkdir(tmpDir);
+      await RNFS.copyFile(AppInfo.sourceDir, tmpApkPath);
+      if (!didNavigateAway.current) {
+        await Share.open({
+          url: "file://" + tmpApkPath,
+          type: "application/vnd.android.package-archive",
+          failOnCancel: false
+        });
+      }
+    } catch (e) {
+      console.error("Error sharing APK", e);
+    } finally {
+      await RNFS.unlink(tmpDir);
+      setStatus("idle");
+    }
+  }, []);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      didNavigateAway.current = false;
+      return () => (didNavigateAway.current = true);
+    }, [])
+  );
+
   return (
-    <List>
-      <ListItem onPress={() => navigate("LanguageSettings")}>
-        <ListItemIcon iconName="language" />
-        <ListItemText
-          primary={<FormattedMessage {...m.language} />}
-          secondary={<FormattedMessage {...m.languageDesc} />}></ListItemText>
-      </ListItem>
-      <ListItem onPress={() => navigate("ProjectConfig")}>
-        <ListItemIcon iconName="assignment" />
-        <ListItemText
-          primary={<FormattedMessage {...m.projectConfig} />}
-          secondary={
-            <FormattedMessage {...m.projectConfigDesc} />
-          }></ListItemText>
-      </ListItem>
-    </List>
+    <>
+      <List>
+        <ListItem onPress={() => navigate("LanguageSettings")}>
+          <ListItemIcon iconName="language" />
+          <ListItemText
+            primary={<FormattedMessage {...m.language} />}
+            secondary={<FormattedMessage {...m.languageDesc} />}></ListItemText>
+        </ListItem>
+        <ListItem onPress={() => navigate("ProjectConfig")}>
+          <ListItemIcon iconName="assignment" />
+          <ListItemText
+            primary={<FormattedMessage {...m.projectConfig} />}
+            secondary={
+              <FormattedMessage {...m.projectConfigDesc} />
+            }></ListItemText>
+        </ListItem>
+        <ListItem onPress={shareApk}>
+          <ListItemIcon iconName="get-app" />
+          <ListItemText
+            primary={<FormattedMessage {...m.appShare} />}
+            secondary={<FormattedMessage {...m.appShareDesc} />}></ListItemText>
+        </ListItem>
+      </List>
+      {status === "loading" ? (
+        <View
+          style={{
+            ...StyleSheet.absoluteFill,
+            backgroundColor: "rgba(0,0,0,0.5)",
+            alignItems: "center",
+            justifyContent: "center"
+          }}>
+          <ActivityIndicator size="large" color="white" />
+        </View>
+      ) : null}
+    </>
   );
 };
 
