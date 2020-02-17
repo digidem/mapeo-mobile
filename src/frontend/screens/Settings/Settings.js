d169df6522948d67ec2b7ab6a06680335782b8d0
Gregor MacLennan <gmaclennan@digital-democracy.org>
2020-02-17 17:17:14 +0000

    Add share sheet for sharing Mapeo APK

--- src/frontend/screens/Settings/Settings.js
+++ src/frontend/screens/Settings/Settings.js
@@ -2,8 +2,12 @@
 import React from "react";
 // import { Picker as OriginalPicker } from "@react-native-community/picker";
 import { FormattedMessage, defineMessages } from "react-intl";
-import { useNavigation } from "react-navigation-hooks";
+import { useNavigation, useFocusEffect } from "react-navigation-hooks";
+import RNFS from "react-native-fs";
+import Share from "react-native-share";
+import DeviceInfo from "react-native-device-info";
 
+import AppInfo from "../../lib/AppInfo";
 import HeaderTitle from "../../sharedComponents/HeaderTitle";
 import {
   List,
@@ -11,6 +15,7 @@ import {
   ListItemText,
   ListItemIcon,
 } from "../../sharedComponents/List";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
 
 const m = defineMessages({
   settingsTitle: {
@@ -48,10 +53,58 @@ const m = defineMessages({
     defaultMessage: "Version and build number",
     description: "Description of the 'About Mapeo' page",
   },
+  appShare: {
+    id: "screens.Settings.appShare",
+    defaultMessage: "Share Mapeo Installer",
+    description: "Primary text for sharing the mapeo APK installer"
+  },
+  appShareDesc: {
+    id: "screens.Settings.appShareDesc",
+    defaultMessage: "Install or update Mapeo on another phone",
+    description: "Secondary text for sharing the mapeo APK installer"
+  }
 });
+
+const APP_VERSION = DeviceInfo.getVersion();
 
 const Settings = () => {
   const { navigate } = useNavigation();
+  const [status, setStatus] = React.useState("idle");
+  const didNavigateAway = React.useRef(false);
+
+  const shareApk = React.useCallback(async () => {
+    const tmpDir = RNFS.DocumentDirectoryPath + "/installer";
+    const apkName = `Mapeo-v${APP_VERSION}.apk`;
+    const tmpApkPath = `${tmpDir}/${apkName}`;
+    setStatus("loading");
+    try {
+      // The apk cannot be shared with other apps from the sourceDir folder, so
+      // we need to create a copy in a place that can be shared. We delete the
+      // copy afterwards
+      await RNFS.mkdir(tmpDir);
+      await RNFS.copyFile(AppInfo.sourceDir, tmpApkPath);
+      if (!didNavigateAway.current) {
+        await Share.open({
+          url: "file://" + tmpApkPath,
+          type: "application/vnd.android.package-archive",
+          failOnCancel: false
+        });
+      }
+    } catch (e) {
+      console.error("Error sharing APK", e);
+    } finally {
+      await RNFS.unlink(tmpDir);
+      setStatus("idle");
+    }
+  }, []);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      didNavigateAway.current = false;
+      return () => (didNavigateAway.current = true);
+    }, [])
+  );
+
   return (
     <List testID="settingsList">
       <ListItem
@@ -83,6 +136,12 @@ const Settings = () => {
           primary={<FormattedMessage {...m.aboutMapeo} />}
           secondary={<FormattedMessage {...m.aboutMapeoDesc} />}
         ></ListItemText>
+      </ListItem>
+      <ListItem onPress={shareApk}>
+        <ListItemIcon iconName="get-app" />
+        <ListItemText
+          primary={<FormattedMessage {...m.appShare} />}
+          secondary={<FormattedMessage {...m.appShareDesc} />}></ListItemText>
       </ListItem>
     </List>
   );
