53f4083dc29ef4e84924a88b762529dc4aa5c677
Gregor MacLennan <gmaclennan@digital-democracy.org>
2020-07-08 11:49:44 +0100

    Clean up syntax and wait for buttons before tapping them.

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,37 +1,59 @@
 /* eslint-env detox/detox, jest/globals */
 
-const waitForVisible = async (element, timeout = 5000) => {
-  waitFor(element)
+const { byId, byText } = require("./matcher");
+
+const actionMethods = [
+  "tap",
+  "multiTap",
+  "longPress",
+  "swipe",
+  "pinch",
+  "scroll",
+  "scrollTo",
+  "typeText",
+  "replaceText",
+  "clearText",
+  "tapReturnKey",
+  "tapBackSpace"
+];
+
+// Wait for an element to be visible. Returns a promise with same methods as element
+const waitForVisible = (el, timeout = 5000) => {
+  const waitForPromise = waitFor(el)
     .toBeVisible()
     .withTimeout(timeout);
+  for (const actionName of actionMethods) {
+    waitForPromise[actionName] = async (...args) => {
+      return el[actionName].apply(el, args);
+    };
+  }
+  return waitForPromise;
 };
 const waitForMap = async () => {
-  await waitForVisible(element(by.id("mapboxMapView")));
+  await waitForVisible(byId("mapboxMapView"));
 };
 const waitForCamera = async () => {
-  await waitForVisible(element(by.id("camera")));
+  await waitForVisible(byId("camera"));
 };
 const tapMapAddButton = async () => {
-  await element(by.id("addButtonMap")).tap();
+  await waitForVisible(byId("addButtonMap")).tap();
 };
 const tapCameraAddButton = async () => {
-  await waitForVisible(element(by.id("addButtonCamera")));
-  await element(by.id("addButtonCamera")).tap();
+  await waitForVisible(byId("addButtonCamera")).tap();
 };
-const tapGpsPill = async () => await element(by.id("gpsPillButton")).tap();
+const tapGpsPill = async () => await byId("gpsPillButton").tap();
 const switchToCameraTab = async () => {
-  await element(by.id("tabBarButtonCamera")).tap();
+  await waitForVisible(byId("tabBarButtonCamera")).tap();
 };
 const switchToMapTab = async () => {
-  await element(by.id("tabBarButtonMap")).tap();
+  await waitForVisible(byId("tabBarButtonMap")).tap();
 };
 const navigateToObservationList = async () => {
-  await element(by.id("observationListButton")).tap();
+  await waitForVisible(byId("observationListButton")).tap();
 };
 const navigateToSettings = async () => {
   await navigateToObservationList();
-  await waitForVisible(element(by.id("settingsButton")));
-  await element(by.id("settingsButton")).tap();
+  await waitForVisible(byId("settingsButton")).tap();
 };
 
 describe("Mapeo", () => {
@@ -42,35 +64,35 @@ describe("Mapeo", () => {
   describe("Initial load & home screen", () => {
     test("Shows map when app loads", async () => {
       await waitForMap();
-      await expect(element(by.id("mapboxMapView"))).toBeVisible();
+      await expect(byId("mapboxMapView")).toBeVisible();
     });
 
     test("Touching tab bar icons switches between map & camera", async () => {
       await switchToCameraTab();
       await waitForCamera();
-      await expect(element(by.id("camera"))).toBeVisible();
+      await expect(byId("camera")).toBeVisible();
       await switchToMapTab();
       await waitForMap();
-      await expect(element(by.id("mapboxMapView"))).toBeVisible();
+      await expect(byId("mapboxMapView")).toBeVisible();
     });
   });
 
   describe("Settings", () => {
     test("Clicking observation list then settings icon shows settings screen", async () => {
       await navigateToSettings();
-      await expect(element(by.id("settingsList"))).toBeVisible();
+      await expect(byId("settingsList")).toBeVisible();
     });
 
     test("Changing language in settings changes app language", async () => {
       await navigateToSettings();
-      await element(by.id("settingsLanguageButton")).tap();
-      await waitFor(element(by.id("esLanguageButton")))
+      await byId("settingsLanguageButton").tap();
+      await waitFor(byId("esLanguageButton"))
         .toBeVisible()
         .whileElement(by.id("languageScrollView"))
         .scroll(100, "down");
-      await element(by.id("esLanguageButton")).tap();
+      await byId("esLanguageButton").tap();
       await navigateToObservationList();
-      await expect(element(by.text("Observaciones"))).toBeVisible();
+      await expect(byText("Observaciones")).toBeVisible();
       // Delete and re-install the app after changing language, so that it
       // resets to the default language
       await device.launchApp({ delete: true });
@@ -80,7 +102,7 @@ describe("Mapeo", () => {
   describe("Create observation", () => {
     test("Tapping add from map screen shows 'choose what is happening' screen", async () => {
       await tapMapAddButton();
-      await expect(element(by.text("Choose what is happening"))).toBeVisible();
+      await expect(byText("Choose what is happening")).toBeVisible();
     });
 
     test("Tapping add from camera screen shows 'choose what is happening' screen", async () => {
@@ -88,16 +110,16 @@ describe("Mapeo", () => {
       await tapCameraAddButton();
       // Need to wait for this screen because the camera shutter button is async
       // and does not switch screens until the photo is taken
-      await waitForVisible(element(by.text("Choose what is happening")), 10000);
-      await expect(element(by.text("Choose what is happening"))).toBeVisible();
+      await waitForVisible(byText("Choose what is happening"), 10000);
+      await expect(byText("Choose what is happening")).toBeVisible();
     });
   });
 
   describe("GPS", () => {
     test("Clicking GPS pill shows GPS screen", async () => {
       await tapGpsPill();
-      await expect(element(by.text("Current GPS Location"))).toBeVisible();
-      await expect(element(by.id("gpsScreenScrollView"))).toBeVisible();
+      await expect(byText("Current GPS Location")).toBeVisible();
+      await expect(byId("gpsScreenScrollView")).toBeVisible();
     });
   });
 });
