403525ab0a9f1392dcbd7aa52db8b7446c7619ce
Gregor MacLennan <gmaclennan@digital-democracy.org>
2020-08-06 12:43:53 +0100

    chore: Patch react-native so gradle tasks package ICCA variant
    Adds the app variant name as an environment variable in the gradle build
    script so that metro bundler can package JS files according to variant
    by using the `sourceExts` option.

--- metro.config.js
+++ metro.config.js
@@ -8,21 +8,32 @@ const blacklist = require("metro-config/src/defaults/blacklist");
 const defaultSourceExts = require("metro-config/src/defaults/defaults")
   .sourceExts;
 
-console.log("METRO", process.env.RN_SRC_EXT);
+const customSourceExts = [];
+
+if (process.env.APP_VARIANT) {
+  const match = process.env.APP_VARIANT.match(/^[^A-Z]*/);
+  if (match) {
+    customSourceExts.push(match[0] + ".js");
+  }
+}
+
+if (process.env.RN_SRC_EXT) {
+  process.env.RN_SRC_EXT.split(",").forEach(ext => {
+    customSourceExts.push(ext);
+  });
+}
 
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false
-      }
-    })
+        inlineRequires: false,
+      },
+    }),
   },
   resolver: {
     blacklistRE: blacklist([/nodejs-assets\/.*/, /android\/.*/, /ios\/.*/]),
-    sourceExts: process.env.RN_SRC_EXT
-      ? process.env.RN_SRC_EXT.split(",").concat(defaultSourceExts)
-      : defaultSourceExts
-  }
+    sourceExts: customSourceExts.concat(defaultSourceExts),
+  },
 };
