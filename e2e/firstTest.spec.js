eeb2d55637d37e3a15845da00f9947ff31c2fd76
Gregor MacLennan <gmaclennan@digital-democracy.org>
2020-09-26 13:42:46 +0100

    cleanup and speedup tests

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -27,32 +27,6 @@ const waitForVisible = (el, timeout = 5000) => {
   }
   return waitForPromise;
 };
-const waitForMap = async () => {
-  await waitForVisible(byId("mapboxMapView"));
-};
-const waitForCamera = async () => {
-  await waitForVisible(byId("camera"));
-};
-const tapMapAddButton = async () => {
-  await waitForVisible(byId("addButtonMap")).tap();
-};
-const tapCameraAddButton = async () => {
-  await waitForVisible(byId("addButtonCamera")).tap();
-};
-const tapGpsPill = async () => await byId("gpsPillButton").tap();
-const switchToCameraTab = async () => {
-  await waitForVisible(byId("tabBarButtonCamera")).tap();
-};
-const switchToMapTab = async () => {
-  await waitForVisible(byId("tabBarButtonMap")).tap();
-};
-const navigateToObservationList = async () => {
-  await waitForVisible(byId("observationListButton")).tap();
-};
-const navigateToSettings = async () => {
-  await navigateToObservationList();
-  await waitForVisible(byId("settingsButton")).tap();
-};
 
 describe("Mapeo", () => {
   beforeEach(async () => {
@@ -61,35 +35,38 @@ describe("Mapeo", () => {
 
   describe("Initial load & home screen", () => {
     test("Shows map when app loads", async () => {
-      await waitForMap();
       await expect(byId("mapboxMapView")).toBeVisible();
     });
 
     test("Touching tab bar icons switches between map & camera", async () => {
-      await switchToCameraTab();
-      await waitForCamera();
+      await byId("tabBarButtonCamera").tap();
       await expect(byId("camera")).toBeVisible();
-      await switchToMapTab();
-      await waitForMap();
+      await byId("tabBarButtonMap").tap();
       await expect(byId("mapboxMapView")).toBeVisible();
     });
   });
 
   describe("Settings", () => {
     test("Clicking observation list then settings icon shows settings screen", async () => {
-      await navigateToSettings();
+      await byId("observationListButton").tap();
+      await byId("settingsButton").tap();
       await expect(byId("settingsList")).toBeVisible();
     });
 
     test("Changing language in settings changes app language", async () => {
-      await navigateToSettings();
+      await byId("observationListButton").tap();
+      await byId("settingsButton").tap();
       await byId("settingsLanguageButton").tap();
       await waitFor(byId("esLanguageButton"))
         .toBeVisible()
         .whileElement(by.id("languageScrollView"))
         .scroll(100, "down");
       await byId("esLanguageButton").tap();
-      await navigateToObservationList();
+      // In e2e test env, when the app reloads (which is what happens when you
+      // change language) the navigation state is lost and we return to the map
+      // screen. In production navigation state is persisted and changing
+      // language stays on the same screen
+      await byId("observationListButton").tap();
       await expect(byText("Observaciones")).toBeVisible();
       // Delete and re-install the app after changing language, so that it
       // resets to the default language
@@ -99,15 +76,15 @@ describe("Mapeo", () => {
 
   describe("Create observation", () => {
     test("Tapping add from map screen shows 'choose what is happening' screen", async () => {
-      await tapMapAddButton();
+      await byId("addButtonMap").tap();
       await expect(byText("Choose what is happening")).toBeVisible();
     });
 
     // expo-camera does not work in an emulator see
     // https://github.com/expo/expo/issues/5529
     test.skip("Tapping add from camera screen shows 'choose what is happening' screen", async () => {
-      await switchToCameraTab();
-      await tapCameraAddButton();
+      await byId("tabBarButtonCamera").tap();
+      await byId("addButtonCamera").tap();
       // Need to wait for this screen because the camera shutter button is async
       // and does not switch screens until the photo is taken
       await waitForVisible(byText("Choose what is happening"), 10000);
@@ -117,7 +94,7 @@ describe("Mapeo", () => {
 
   describe("GPS", () => {
     test("Clicking GPS pill shows GPS screen", async () => {
-      await tapGpsPill();
+      await byId("gpsPillButton").tap();
       await expect(byText("Current GPS Location")).toBeVisible();
       await expect(byId("gpsScreenScrollView")).toBeVisible();
     });
