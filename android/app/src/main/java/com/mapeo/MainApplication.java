06fe1b26563a9c9aa979dd94a9b4ed69df4874a8
Gregor MacLennan <gmaclennan@digital-democracy.org>
2021-04-15 15:41:54 +0100

    fix: Add missing symbol to Java code (ReactInstanceManager)

--- android/app/src/main/java/com/mapeo/MainApplication.java
+++ android/app/src/main/java/com/mapeo/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
 import com.bugsnag.BugsnagReactNative;
 import cl.json.ShareApplication;
 import com.facebook.react.ReactNativeHost;
