ab3be5a68f43c58543ad74a30fc459f40e529ff0
luandro <luandro@gmail.com>
2021-07-16 14:13:58 -0300

    Add react-testing-library and Jest suite (#519)
    * babel typescript compilations
    
    * test: Start adding react-testing-library and Jest suite
    
    * chore: Add manual mocks for api, AppLoading, ConfigContext, ObservationContext, AsyncStorage, react-native-area-views, react-navigation
    
    * fix: Fix testing framework setup
    
    * Fix api tests
    
    * Remove babel typescript transform
    
    * New Tests
    
    * Simple Test
    
    * Update package-lock.json
    
    * Update api.spec.js
    
    * Update Settings.test.tsx
    
    * Fix haste map warnings
    
    * Use legacy fake timers
    
    This was a breaking change from Jest@27
    https://jestjs.io/blog/2020/05/05/jest-26#new-fake-timers
    
    * Basic test for settings screen with navigate
    
    * Mock expo-document-picker to remove warning
    
    * render category chooser with providers
    
    * Use default translated text for selectors, rather than testID
    
    see https://callstack.github.io/react-native-testing-library/docs/how-should-i-query
    
    * Properly fix the haste-map issue by using the correct config.
    
    To test this works, you need to run `npm test -- --clearCache`
    because Jest only shows the haste error on first run on any machine.
    
    * Turn off debug logging in api.spec.js
    
    Co-authored-by: ErikSin <67773827+ErikSin@users.noreply.github.com>
    Co-authored-by: Gregor MacLennan <gmaclennan@digital-democracy.org>

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+const modulesToTransform = [
+  "react-native",
+  "@react-native(-community)?",
+  "react-native-splash-screen",
+  "ky",
+  "react-native-fs",
+  "react-native-reanimated",
+  "@react-navigation",
+  "react-navigation",
+  "react-native-gesture-handler",
+  "react-native-iphone-x-helper",
+  "react-navigation-stack",
+  "react-native-vector-icons",
+  "@react-native-mapbox-gl",
+  "expo-document-picker",
+  "@unimodules",
+];
+
+module.exports = {
+  preset: "react-native",
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  testPathIgnorePatterns: ["/node_modules/", "/e2e/", "/src/backend/"],
+  modulePathIgnorePatterns: [
+    "/nodejs-assets/",
+    // Because we compile the backend and include in the app, package.json and
+    // others will be in the android and ios folders and cause Jest to complain
+    "/android/",
+    "/ios/",
+  ],
+  transformIgnorePatterns: [
+    `/node_modules/(?!(${modulesToTransform.join("|")})/)`,
+  ],
+  setupFiles: ["<rootDir>/jest/setup.js"],
+};
