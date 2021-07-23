01ab7187821f91f2f33f96542a5eb7cc10519d96
achou11 <andrewchou@fastmail.com>
2021-07-23 14:39:19 -0400

    Create custom BottomSheet component

---
+++ src/frontend/sharedComponents/BottomSheet/Backdrop.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import Animated, { Extrapolate, interpolate } from "react-native-reanimated";
+import { BottomSheetBackdropProps } from "@gorhom/bottom-sheet";
+
+import { BLACK } from "../../lib/styles";
+
+const Backdrop = ({ animatedIndex, style }: BottomSheetBackdropProps) => {
+  const animatedOpacity = React.useMemo(
+    () =>
+      interpolate(animatedIndex, {
+        inputRange: [0, 1],
+        outputRange: [0, 0.3],
+        extrapolate: Extrapolate.CLAMP,
+      }),
+    [animatedIndex]
+  );
+
+  const containerStyle = React.useMemo(
+    () => [
+      style,
+      {
+        backgroundColor: BLACK,
+        opacity: animatedOpacity,
+      },
+    ],
+    [style, animatedOpacity]
+  );
+
+  return <Animated.View style={containerStyle} />;
+};
+
+export default Backdrop;
