5d6713091c71c872277b7632db8d6ea443690fed
ErikSin <67773827+ErikSin@users.noreply.github.com>
2021-07-26 13:56:49 -0700

    Update AppContainer.tsx

--- src/frontend/AppContainer.tsx
+++ src/frontend/AppContainer.tsx
@@ -1,4 +1,16 @@
 import AppStack from "./NavigationStacks/AppStack";
 import { createAppContainer } from "react-navigation";
+import { createSwitchNavigator } from "react-navigation";
+import { OnboardingStack } from "./NavigationStacks/OnboardingStack";
 
-export default createAppContainer(AppStack);
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      Onboarding: OnboardingStack,
+      App: AppStack,
+    },
+    {
+      initialRouteName: "Onboarding",
+    }
+  )
+);
