1d6832dd7ff61e78468929652aa4751abd5e137e
Andrew Chou <andrewchou@fastmail.com>
2021-08-12 14:27:13 -0400

    Implement Add to Project flow (#682)
    * Update various expo deps
    
    * Implment qr scan step
    
    * Implement device found step
    
    * Small updates
    
    * Get project name from context for success step
    
    * Refactor DeviceFoundStep to use radio buttons
    
    * Remove unnecessary import
    
    * Fix navigation transition lag for QR scan step
    
    * Fix text alignment in scan step
    
    * Fix issue with camera loading when going back from found step
    
    * Fix text overflow issues in found step
    
    * Move AddToProject to AppStack
    
    * Stack action buttons in found step
    
    * Fix button styling in success step
    
    * Add some missing unimodules android configuration

---
+++ src/frontend/screens/AddToProjectScreen/DeviceFoundStep/OptionRow.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { View, StyleSheet } from "react-native";
+import { TouchableNativeFeedback } from "../../../sharedComponents/Touchables";
+import { VERY_LIGHT_BLUE } from "../../../lib/styles";
+
+interface Props extends React.PropsWithChildren<{}> {
+  onPress: () => void;
+}
+
+export const OptionRow = ({ children, onPress }: Props) => (
+  <View style={styles.optionContainer}>
+    <View style={styles.pressableContainer}>
+      <TouchableNativeFeedback
+        onPress={onPress}
+        background={TouchableNativeFeedback.Ripple(VERY_LIGHT_BLUE, false)}
+      >
+        {children}
+      </TouchableNativeFeedback>
+    </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  optionContainer: {
+    flexDirection: "row",
+    marginBottom: 16,
+  },
+  pressableContainer: {
+    flex: 1,
+  },
+});
