d82ecb22653b50e66f1b7d344d40fee85789c6ce
achou11 <andrewchou@fastmail.com>
2021-08-23 14:57:36 -0400

    chore: Add comment for components enabled by onboarding flag

--- src/frontend/screens/Onboarding/JoinProjectQrScreen.tsx
+++ src/frontend/screens/Onboarding/JoinProjectQrScreen.tsx
@@ -1,3 +1,9 @@
+/**
+ * Only reachable if the `onboarding` experiment is enabled
+ * by doing either of the following:
+ *   - Set `FEATURE_ONBOARDING=true` when running/building
+ *   - Manually change the context value in `SettingsContext.tsx`
+ */
 import * as React from "react";
 import { StyleSheet, View } from "react-native";
 import { useNavigation } from "react-navigation-hooks";
