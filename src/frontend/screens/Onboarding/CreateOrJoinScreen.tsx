6530dfa433236ac70e998d17d31f6d21cf1334e5
achou11 <andrewchou@fastmail.com>
2021-08-26 12:55:07 -0400

    chore: update UI for CreateOrJoinScreen component

--- src/frontend/screens/Onboarding/CreateOrJoinScreen.tsx
+++ src/frontend/screens/Onboarding/CreateOrJoinScreen.tsx
@@ -4,101 +4,135 @@
  *   - Set `FEATURE_ONBOARDING=true` when running/building
  *   - Manually change the context value in `SettingsContext.tsx`
  */
-import React from "react";
-import { View, Text, StyleSheet, Image, Alert } from "react-native";
+import * as React from "react";
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
 import { useNavigation } from "react-navigation-hooks";
-import { defineMessages, FormattedMessage } from "react-intl";
-import {
-  NavigationStackOptions,
-  NavigationStackScreenComponent,
-} from "react-navigation-stack";
+import { FormattedMessage, defineMessages } from "react-intl";
+import { NavigationStackScreenComponent } from "react-navigation-stack";
+import MaterialIcon from "react-native-vector-icons/MaterialIcons";
 
+import { BLACK, MAPEO_BLUE, WHITE } from "../../lib/styles";
 import Button from "../../sharedComponents/Button";
 
 const m = defineMessages({
-  joinProject: {
-    id: "screens.Onboarding.CreateOrJoinProjectScreen.joinProject",
-    defaultMessage: "Join a Project",
-  },
   createProject: {
     id: "screens.Onboarding.CreateOrJoinProjectScreen.createProject",
     defaultMessage: "Create a Project",
   },
+  joinProject: {
+    id: "screens.Onboarding.CreateOrJoinProjectScreen.joinProject",
+    defaultMessage: "Join a Project",
+  },
+  practiceMode: {
+    id: "screens.Onboarding.CreateOrJoinProjectScreen.praceticeMode",
+    defaultMessage: "Practice Mode",
+  },
+  joinCta: {
+    id: "screens.Onboarding.CreateOrJoinProjectScreen.joinCta",
+    defaultMessage: "Tap here to {action}",
+  },
 });
 
-const navOptions: NavigationStackOptions = {
-  headerShown: false,
+const showWipAlert = () => {
+  Alert.alert("Work in progress", "This feature has not been implemented yet", [
+    {
+      text: "Ok",
+      onPress: () => {},
+    },
+  ]);
 };
 
 export const CreateOrJoinScreen: NavigationStackScreenComponent = () => {
   const { navigate } = useNavigation();
 
   return (
-    <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Image source={require("../../images/icon_mapeo_pin.png")} />
-      </View>
+    <View style={styles.pageContainer}>
+      <View style={styles.primaryActionsContainer}>
+        <View style={styles.logoContainer}>
+          <Image source={require("../../images/icon_mapeo_pin.png")} />
+        </View>
 
-      <Text style={styles.title}>Mapeo</Text>
-      <Button
-        style={styles.button}
-        onPress={() => {
-          navigate("JoinProjectQr");
-        }}
-      >
-        <Text style={styles.buttonText}>
-          <FormattedMessage {...m.joinProject} />
-        </Text>
-      </Button>
-      <Button
-        style={styles.button}
-        onPress={() => {
-          Alert.alert(
-            "Work in progress",
-            "This feature has not been implemented yet",
-            [
-              {
-                text: "Ok",
-                onPress: () => {},
-              },
-            ]
-          );
-        }}
-      >
-        <Text style={styles.buttonText}>
-          <FormattedMessage {...m.createProject} />
+        <Text style={styles.title}>Mapeo</Text>
+        <Button fullWidth onPress={showWipAlert} style={styles.button}>
+          <Text style={[styles.buttonText, { color: WHITE }]}>
+            <FormattedMessage {...m.createProject} />
+          </Text>
+        </Button>
+        <Button
+          fullWidth
+          onPress={showWipAlert}
+          style={styles.button}
+          variant="outlined"
+        >
+          <View style={styles.practiceModeContentContainer}>
+            <View style={styles.lightBulbIconContainer}>
+              <MaterialIcon
+                name="wb-incandescent"
+                color={MAPEO_BLUE}
+                size={22}
+                style={styles.lightBulbIcon}
+              />
+            </View>
+            <Text style={[styles.buttonText, { color: MAPEO_BLUE }]}>
+              <FormattedMessage {...m.practiceMode} />
+            </Text>
+          </View>
+        </Button>
+      </View>
+      <Button onPress={() => navigate("JoinProjectQr")} variant="text">
+        <Text style={[styles.buttonText, { color: BLACK }]}>
+          <FormattedMessage
+            {...m.joinCta}
+            values={{
+              action: (
+                <Text style={{ color: MAPEO_BLUE }}>
+                  <FormattedMessage {...m.joinProject} />
+                </Text>
+              ),
+            }}
+          />
         </Text>
       </Button>
     </View>
   );
 };
 
-//TODO research typesafe option
-CreateOrJoinScreen.navigationOptions = navOptions;
+CreateOrJoinScreen.navigationOptions = {
+  headerShown: false,
+};
 
 const styles = StyleSheet.create({
-  title: {
-    fontSize: 45,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginTop: 10,
-    marginBottom: 30,
-  },
-  container: {
-    justifyContent: "center",
+  pageContainer: {
     flex: 1,
+    padding: 40,
+  },
+  primaryActionsContainer: {
+    flex: 1,
+    justifyContent: "center",
   },
   logoContainer: {
-    justifyContent: "center",
     alignItems: "center",
   },
+  title: {
+    fontSize: 60,
+    textAlign: "center",
+    marginBottom: 20,
+    fontWeight: "700",
+  },
   button: {
-    margin: 15,
-    fontSize: 15,
+    marginBottom: 20,
   },
   buttonText: {
     fontSize: 18,
-    color: "#FFFFFF",
-    fontWeight: "bold",
+    fontWeight: "700",
+  },
+  practiceModeContentContainer: {
+    flexDirection: "row",
+  },
+  lightBulbIconContainer: {
+    marginRight: 10,
+  },
+  lightBulbIcon: {
+    transform: [{ rotate: "180deg" }],
   },
 });
