2027f2a4eac7a15f3909a2fc70064672a8c1ba18
achou11 <andrewchou@fastmail.com>
2021-12-02 16:22:49 -0500

    Fix incorrect integration of new Bugsnag API

--- src/frontend/lib/logger.js
+++ src/frontend/lib/logger.js
@@ -1,15 +1,21 @@
-import { Client, Configuration } from "@bugsnag/react-native";
+import Bugsnag from "@bugsnag/react-native";
 import semverPrerelease from "semver/functions/prerelease";
 import { version } from "../../../package.json";
 
-const config = new Configuration("572d472ea9d5a9199777b88ef268da4e");
-const prereleaseComponents = semverPrerelease(version);
-config.releaseStage = __DEV__
-  ? "development"
-  : prereleaseComponents
-  ? prereleaseComponents[0]
-  : "production";
-config.appVersion = version;
-const bugsnag = new Client(config);
+let initialized = false;
 
-export default bugsnag;
+if (!initialized) {
+  const prereleaseComponents = semverPrerelease(version);
+
+  Bugsnag.start({
+    releaseStage: __DEV__
+      ? "development"
+      : prereleaseComponents
+      ? prereleaseComponents[0]
+      : "production",
+  });
+
+  initialized = true;
+}
+
+export default Bugsnag;
