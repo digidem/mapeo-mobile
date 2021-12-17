a873b72d3ac9a202044996e89bb88192e7f18d5d
achou11 <andrewchou@fastmail.com>
2021-12-17 14:27:11 -0500

    Initial pass at setting Bugsnag release stage at build time

--- src/frontend/lib/logger.ts
+++ src/frontend/lib/logger.ts
@@ -1,22 +1,5 @@
 import Bugsnag from "@bugsnag/react-native";
-// import semverPrerelease from "semver/functions/prerelease";
-// import { version } from "../../../package.json";
 
-// const prereleaseComponents = semverPrerelease(version);
-
-Bugsnag.start({
-  /**
-   * TODO(andrew): release stage cannot be set from JS layer and must be done at native layer
-   *
-   * https://github.com/bugsnag/bugsnag-js/issues/1264#issuecomment-767191963
-   * https://github.com/bugsnag/bugsnag-js/issues/1263#issuecomment-767193544
-   * https://github.com/bugsnag/bugsnag-js/issues/1575#issuecomment-965002319
-   */
-  // releaseStage: __DEV__
-  //   ? "development"
-  //   : prereleaseComponents
-  //   ? prereleaseComponents[0]
-  //   : "production",
-});
+Bugsnag.start();
 
 export default Bugsnag;
