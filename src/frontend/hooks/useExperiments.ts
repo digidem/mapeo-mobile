4e40b4921721c559731a257f50184fe608a426a9
ErikSin <67773827+ErikSin@users.noreply.github.com>
2022-05-04 11:55:34 -0700

    chore updated hook

--- src/frontend/hooks/useExperiments.ts
+++ src/frontend/hooks/useExperiments.ts
@@ -10,7 +10,7 @@ export function useExperiments() {
     (key: keyof SettingsState["experiments"], val: boolean) => {
       setSettings("experiments", { ...settings.experiments, [key]: val });
     },
-    [setSettings]
+    [setSettings, settings.experiments]
   );
 
   return [settings.experiments, setExperiments] as const;
