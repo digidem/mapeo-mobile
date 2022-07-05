16907a5917ba75ecc7345c6a2047a1f22b2c28f9
ErikSin <67773827+ErikSin@users.noreply.github.com>
2022-07-04 17:50:11 -0700

    test: update front end test

--- src/frontend/screens/__tests__/CategoryChooser.test.tsx
+++ src/frontend/screens/__tests__/CategoryChooser.test.tsx
@@ -6,6 +6,8 @@ import CategoryChooser from "../CategoryChooser";
 
 // CategoryChooser uses useDraftObservation which uses the api module
 jest.mock("../../api");
+jest.mock("../../hooks/useNavigationWithTypes");
+jest.mock("../../hooks/useSetHeader");
 
 test("examples of some things", async () => {
   const chooser = render(<CategoryChooser navigation={{}} />);
