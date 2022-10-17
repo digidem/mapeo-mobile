a217aa685c0fcf3adf448abd5134c7c94a09f56a
ErikSin <67773827+ErikSin@users.noreply.github.com>
2022-10-17 15:36:25 -0700

    chore: adjust some styling

--- src/frontend/screens/AppPasscode/InputPasscode.tsx
+++ src/frontend/screens/AppPasscode/InputPasscode.tsx
@@ -127,7 +127,7 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     height: "100%",
     flex: 1,
-    justifyContent: "space-between",
+    justifyContent: "flex-start",
   },
   error: {
     textAlign: "center",
