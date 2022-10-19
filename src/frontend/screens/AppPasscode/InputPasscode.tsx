3bb6eba2a6b89f322658c45c825c53d931725783
ErikSin <67773827+ErikSin@users.noreply.github.com>
2022-10-19 14:06:32 -0700

    fix: ui hidden error messages

--- src/frontend/screens/AppPasscode/InputPasscode.tsx
+++ src/frontend/screens/AppPasscode/InputPasscode.tsx
@@ -4,10 +4,15 @@ import {
   FormattedMessage,
   MessageDescriptor,
 } from "react-intl";
-import { View, StyleSheet, Text, ScrollView } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ScrollView,
+  KeyboardAvoidingView,
+} from "react-native";
 import { useBlurOnFulfill } from "react-native-confirmation-code-field";
-import { useNavigationFromRoot } from "../../hooks/useNavigationWithTypes";
-import { WHITE, RED, MAPEO_BLUE } from "../../lib/styles";
+import { WHITE, RED } from "../../lib/styles";
 import Button from "../../sharedComponents/Button";
 import {
   CELL_COUNT,
@@ -18,10 +23,6 @@ const m = defineMessages({
   button: {
     id: "screens.AppPasscode.NewPasscode.InputPasscodeScreen.button",
     defaultMessage: "Next",
-  },
-  cancel: {
-    id: "screens.AppPasscode.InputPasscodeScreen.cancel",
-    defaultMessage: "Cancel",
   },
 });
 interface InputPasscodeProps {
@@ -52,58 +53,43 @@ export const InputPasscode = ({
 
   if (error) inputRef.current?.focus();
 
-  const { goBack } = useNavigationFromRoot();
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View>
-          <Text style={[styles.header]}>
-            <FormattedMessage {...text.title} />
+    <View style={[styles.container]}>
+      <View>
+        <Text style={[styles.header]}>
+          <FormattedMessage {...text.title} />
+        </Text>
+        <Text style={[styles.subtext]}>
+          <FormattedMessage {...text.subtitle} />
+        </Text>
+
+        <PasscodeInput
+          error={error}
+          ref={inputRef}
+          inputValue={inputValue}
+          onChangeTextWithValidation={setInputValue}
+          maskValues={!showPasscodeValues}
+        />
+
+        {error && (
+          <Text style={styles.error}>
+            <FormattedMessage {...text.errorMessage} />
           </Text>
-          <Text style={[styles.subtext]}>
-            <FormattedMessage {...text.subtitle} />
-          </Text>
-
-          <PasscodeInput
-            error={error}
-            ref={inputRef}
-            inputValue={inputValue}
-            onChangeTextWithValidation={setInputValue}
-            maskValues={!showPasscodeValues}
-          />
-
-          {error && (
-            <Text style={styles.error}>
-              <FormattedMessage {...text.errorMessage} />
-            </Text>
-          )}
-        </View>
-
-        <View>
-          <Button
-            fullWidth
-            variant="outlined"
-            style={{ marginBottom: 20, marginTop: 20 }}
-            onPress={goBack}
-          >
-            <Text style={[styles.buttonText, { color: MAPEO_BLUE }]}>
-              <FormattedMessage {...m.cancel} />
-            </Text>
-          </Button>
-
-          <Button fullWidth onPress={validate}>
-            <Text style={[styles.buttonText, { color: WHITE }]}>
-              <FormattedMessage {...m.button} />
-            </Text>
-          </Button>
-        </View>
+        )}
       </View>
-    </ScrollView>
+
+      <Button fullWidth onPress={validate}>
+        <Text style={styles.buttonText}>
+          <FormattedMessage {...m.button} />
+        </Text>
+      </Button>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   buttonText: {
+    color: WHITE,
     fontSize: 16,
     textAlign: "center",
   },
@@ -125,9 +111,8 @@ const styles = StyleSheet.create({
   container: {
     padding: 20,
     flexDirection: "column",
-    height: "100%",
     flex: 1,
-    justifyContent: "flex-start",
+    justifyContent: "space-between",
   },
   error: {
     textAlign: "center",
