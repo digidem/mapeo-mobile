a80b874177675519d1f57576979ee5d2264ee630
Gregor MacLennan <gmaclennan@digital-democracy.org>
2022-11-07 10:47:34 +0000

    fix: update to react-native-v8@1.4.8

--- android/app/src/main/java/com/mapeo/MainApplication.java
+++ android/app/src/main/java/com/mapeo/MainApplication.java
@@ -12,6 +12,8 @@ import com.bugsnag.android.Bugsnag;
 import cl.json.ShareApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
+import com.facebook.react.modules.systeminfo.AndroidInfoHelpers;
 import com.facebook.soloader.SoLoader;
 
 import com.mapeo.generated.BasePackageList;
@@ -25,6 +27,8 @@ import com.mapeo.ApkInstallerPackage;
 import java.util.Arrays;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
+import io.csie.kudo.reactnative.v8.executor.V8ExecutorFactory;
 
 public class MainApplication extends Application implements ShareApplication, ReactApplication {
   private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(
@@ -57,6 +61,15 @@ public class MainApplication extends Application implements ShareApplication, Re
     @Override
     protected String getJSMainModuleName() {
       return "index";
+    }
+
+    @Override
+    protected JavaScriptExecutorFactory getJavaScriptExecutorFactory() {
+      return new V8ExecutorFactory(
+        getApplicationContext(),
+        getPackageName(),
+        AndroidInfoHelpers.getFriendlyDeviceName(),
+        getUseDeveloperSupport());
     }
   };
 
