ae8d748407025d7a9e9fea1538322179775cc078
Andrew Chou <andrewchou@fastmail.com>
2022-11-10 16:57:04 -0500

    remove unused import

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import "./src/frontend/lib/logger";
 import "./src/frontend/polyfills";
 
 import { AppRegistry } from "react-native";
-import { polyfillGlobal } from "react-native/Libraries/Utilities/PolyfillFunctions";
 import App from "./src/frontend/App";
 import { name as appName } from "./app.json";
 
