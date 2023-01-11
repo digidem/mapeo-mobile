d919348c592d4f45e55ed52aeca06ed90334a754
Andrew Chou <andrewchou@fastmail.com>
2023-01-11 13:45:33 -0500

    re-enable background maps experiment option in settings

--- src/frontend/context/SettingsContext.tsx
+++ src/frontend/context/SettingsContext.tsx
@@ -62,9 +62,7 @@ export const SettingsProvider = ({ children }: React.PropsWithChildren<{}>) => {
   const contextValue: SettingsContextType = React.useMemo(() => {
     // If we add any new properties to the settings state, they will be
     // undefined in a users' persisted state, so we merge in the defaults
-    const mergedState = merge({}, DEFAULT_SETTINGS, state, {
-      experiments: { backgroundMaps: false },
-    });
+    const mergedState = merge({}, DEFAULT_SETTINGS, state);
     return [mergedState, setSettings];
   }, [state, setSettings]);
 
