fa863846c2cc0af73f785e8d3fdb63befe49e226
Andrew Chou <andrewchou@fastmail.com>
2023-06-15 08:46:10 -0400

    fix typo

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -6,7 +6,7 @@ import { LogBox } from "react-native";
 import SplashScreen from "react-native-splash-screen";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-// We need to wrap the app with this provider to fix an the bottom sheet modal backdrop
+// We need to wrap the app with this provider to fix an issue with the bottom sheet modal backdrop
 // not overlaying the navigation header. Without this, the header is accessible even when
 // the modal is open, which we don't want (e.g. header back button shouldn't be reachable).
 // See https://github.com/gorhom/react-native-bottom-sheet/issues/1157
