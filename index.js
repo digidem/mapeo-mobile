79395c18e093c862b7b2e2fde468c726dbcb39d1
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-06-16 15:00:06 -0700

    take out intl polyfills

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // @flow
 // setup bugsnag before anything else
 import "./src/frontend/lib/logger";
-import "./src/frontend/polyfills";
 
 import { AppRegistry } from "react-native";
 import App from "./src/frontend/App";
