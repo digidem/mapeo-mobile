ffe572a5545ebfd480c3cda5f3d0c1c1e05cdd55
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-03-13 11:31:55 -0700

    chore:gave config access to shell env variable

--- src/frontend/lib/config.ts
+++ src/frontend/lib/config.ts
@@ -1,5 +1,6 @@
 import Config from "react-native-config";
 
 export const config = {
-  MAPBOX_ACCESS_TOKEN: Config.MAPBOX_ACCESS_TOKEN,
+  MAPBOX_ACCESS_TOKEN:
+    Config.MAPBOX_ACCESS_TOKEN || process.env.MAPBOX_ACCESS_TOKEN,
 };
